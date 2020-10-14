949620bfaeb352940349f5b166ee64ace5125a1a
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-10-14 15:20:24 +0530

    Update chatbot.py

--- CutiepiiRobot/modules/chatbot.py
+++ CutiepiiRobot/modules/chatbot.py
@@ -8,9 +8,9 @@ from coffeehouse.exception import CoffeeHouseError as CFError
 from telegram import Message, Chat, User, Update, Bot
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 
-from tg_bot import dispatcher, AI_API_KEY, OWNER_ID
-import tg_bot.modules.sql.chatbot_sql as sql
-from tg_bot.modules.helper_funcs.filters import CustomFilters
+from CutiepiiRobot import dispatcher, AI_API_KEY, OWNER_ID
+import CutiepiiRobot.modules.sql.chatbot_sql as sql
+from CutiepiiRobot.modules.helper_funcs.filters import CustomFilters
 
 
 CoffeeHouseAPI = API(AI_API_KEY)
