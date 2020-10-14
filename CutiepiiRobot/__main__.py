bfb3ddf4e4a0a2f8ad29c286fdd6cbea91a91135
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-10-14 17:15:49 +0530

    Update __main__.py

--- CutiepiiRobot/__main__.py
+++ CutiepiiRobot/__main__.py
@@ -52,14 +52,20 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+
+I am an Anime themed advance group management bot with a lot of Special Features.
+
 You can find my list of available commands with /help.
+
+FREE MOVIES AND SERIES ON [HINDI K DRAMA](https://Hindikdrama.xyz)
+
+Maintained by @WadiwalaTech
+
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
+I'm a heroine For Fun and help admins manage their groups with Cutiepii! Have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:
@@ -77,12 +83,12 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/46e6d9dfcb3eb9eae95d9.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/d1f83886dd1c5d0b3c530.png"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
-You can donate to the original writer of the Base code, Paul
-There are two ways of supporting him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+Cutiepii is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
+You can donate to the original writer of the Base code, Rajkumar
+There are two ways of supporting him; [PayPal](paypal.me/wadwialatech), or [Ko-Fi](ko-fi.com/Rajkumar)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
