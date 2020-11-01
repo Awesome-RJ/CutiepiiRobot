7327244533cf071781f7c914625828917acd3d90
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-11-01 15:51:15 +0530

    Update __main__.py

--- CutiepiiRobot/__main__.py
+++ CutiepiiRobot/__main__.py
@@ -492,27 +492,7 @@ def get_settings(update: Update, context: CallbackContext):
 
     else:
         send_settings(chat.id, user.id, True)
-
-@run_async
-def start(bot: Bot, update: Update, args: List[str]):
-    if update.effective_chat.type == "private":
-        if len(args) >= 1:
-            if args[0].lower() == "help":
-                send_help(update.effective_chat.id, HELP_STRINGS)
-            elif args[0].lower() == "disasters":
-                IMPORTED["disasters"].send_disasters(update)
-            elif args[0].lower().startswith("stngs_"):
-                match = re.match("stngs_(.*)", args[0].lower())
-                chat = dispatcher.bot.getChat(match.group(1))
-
-                if is_user_admin(chat, update.effective_user.id):
-                    send_settings(match.group(1), update.effective_user.id, False)
-                else:
-                    send_settings(match.group(1), update.effective_user.id, True)
-
-            elif args[0][1:].isdigit() and "rules" in IMPORTED:
-                IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
-        
+       
 @run_async
 def donate(update: Update, context: CallbackContext):
     user = update.effective_message.from_user
