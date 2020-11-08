cc3e413338b4d697518051b8e93e3bebb34704ee
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-11-09 00:16:42 +0530

    Update fun_strings.py

--- CutiepiiRobot/modules/fun_strings.py
+++ CutiepiiRobot/modules/fun_strings.py
@@ -1,25 +1,3 @@
-RUN_STRINGS = (
-    "Now you see me, now you don't."
-    "ε=ε=ε=ε=┌(;￣▽￣)┘",
-    "Get back here!",
-    "REEEEEEEEEEEEEEEEEE!!!!!!!",
-    "Look out for the wall!",
-    "Don't leave me alone with them!!",
-    "You've got company!",
-    "Chotto matte!",
-    "Yare yare daze",
-    "*Naruto run activated*",
-    "*Nezuko run activated*",
-    "Hey take responsibilty for what you just did!",
-    "May the odds be ever in your favour.",
-    "Run everyone, they just dropped a bomb 💣💣",
-    "And they disappeared forever, never to be seen again.",
-    "Legend has it, they're still running.",
-    "Hasta la vista, baby.",
-    "Ah, what a waste. I liked that one.",
-    "As The Doctor would say... RUN!",
-)
-
 ABUSE_STRINGS = (
     "Fuck off",
     "Stfu go fuck yourself",
