6c4018fc44e0b9f68a64330dd29dd30060974f3a
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-11-08 17:10:57 +0530

    Update __main__.py

--- CutiepiiRobot/__main__.py
+++ CutiepiiRobot/__main__.py
@@ -208,7 +208,7 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton(
-                            text="☑️ Add Saitama to your group",
+                            text="☑️ Add Cutiepii to your group",
                             url="t.me/{}?startgroup=true".format(
                                 context.bot.username))
                     ],
