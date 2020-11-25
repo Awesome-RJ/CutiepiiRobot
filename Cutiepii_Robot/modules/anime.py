cfd0592582792cf6f6758a2cc6c5c71fd581ed48
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-11-25 15:12:36 +0530

    Update anime.py

--- Cutiepii_Robot/modules/anime.py
+++ Cutiepii_Robot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from SaitamaRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from Cutiepii_Robot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
