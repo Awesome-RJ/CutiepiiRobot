41b57c152b1e07d138f8cb16482ef5eb08dcbff8
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-11-25 14:31:42 +0530

    Create CutieSpecial.py

---
+++ Cutiepii_Robot/modules/CutieSpecial.py
@@ -0,0 +1,18 @@
+
+__help__ = """
+-> `/slap`
+slap a user, or get slapped if not a reply.
+-> `/shrug`
+get shrug XD.
+-> `/table`
+get flip/unflip :v.
+-> `/insult`
+Insults the retar
+-> `/pat`
+pats a user by a reply to the message
+-> `/hug`
+hugs a user by a reply to the message
+"""
+
+
+__mod_name__ = "Cutie"
