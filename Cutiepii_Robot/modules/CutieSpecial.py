57f722ed5448e76b2cc9dd103c752d1682bf0548
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-11-25 15:06:26 +0530

    Update CutieSpecial.py

--- Cutiepii_Robot/modules/CutieSpecial.py
+++ Cutiepii_Robot/modules/CutieSpecial.py
@@ -1,17 +1,7 @@
 
 __help__ = """
--> `/slap`
-slap a user, or get slapped if not a reply.
--> `/shrug`
-get shrug XD.
--> `/table`
-get flip/unflip :v.
--> `/insult`
-Insults the retar
 -> `/pat`
 pats a user by a reply to the message
--> `/hug`
-hugs a user by a reply to the message
 """
 
 
