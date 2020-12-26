c4082e4d6e261af76228a3b9ae56a190043b3e57
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-12-26 22:07:20 +0530

    [batch] Comments and minor edits for batch files

--- Cutiepii_Robot/modules/locks.py
+++ Cutiepii_Robot/modules/locks.py
@@ -9,20 +9,20 @@ from telegram.utils.helpers import mention_html
 
 from alphabet_detector import AlphabetDetector
 
-import SaitamaRobot.modules.sql.locks_sql as sql
-from SaitamaRobot import dispatcher, DRAGONS, LOGGER
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+import Cutiepii_Robot.modules.sql.locks_sql as sql
+from Cutiepii_Robot import dispatcher, DRAGONS, LOGGER
+from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
+from Cutiepii_Robot.modules.helper_funcs.chat_status import (
     can_delete,
     is_user_admin,
     user_not_admin,
     is_bot_admin,
     user_admin,
 )
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.connection import connected
+from Cutiepii_Robot.modules.log_channel import loggable
+from Cutiepii_Robot.modules.connection import connected
 
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from Cutiepii_Robot.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
