d5c42e085c21103a8b4cbc73c86896a9b841be4a
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2021-01-03 11:21:10 +0530

    Update abuse.py

--- Cutiepii_Robot/modules/abuse.py
+++ Cutiepii_Robot/modules/abuse.py
@@ -1,48 +1,29 @@
+import html
 import random
-from telegram.ext import run_async, Filters
-from telegram import Message, Chat, Update, Bot, MessageEntity
+import time
+from typing import List
+
+from telegram import Bot, Update, ParseMode
+from telegram.ext import run_async
+
+import Cutiepii_Robot.modules.fun_strings as fun_strings
 from Cutiepii_Robot import dispatcher
 from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
-
-SFW_STRINGS = (
-   "What the fuck just happened?/What the fuck is going on here?",
-   "(Respectfully), shut the fuck up!",
-   "They may go fuck themselves",
-   "I’m fucking with you",
-   "Your software can choke on my fucking balls",
-   "I’ll show you fucking vision",
-   "fucking prick",
-   "Oh, for fuck’s sake!",
-   "Are you fucking serious right now?",
-   "Let’s fucking do this!",
-   "fuck off",
-   "cool shit!/shit codebase /What a shit circus!/Oh, shit!",
-   "beat the shit out of someone",
-   "look like shit",
-   "You’re fucking piece of shit!",
-   "Why don’t you choke on my ball?",
-   "cock-sucker",
-   "pussy",
-   "You saved my ass",
-   "I was suck a dick to you",
-   "You are working your ass off",
-   "suck his dick",
-   "dickhole",
-   "Now, that’s just one episode full of curse words.",
-   "son of a bitch or SOB",
-   "backstabbing bitch",
-   "Taking the piss",
-)
+from Cutiepii_Robot.modules.helper_funcs.chat_status import is_user_admin
+from Cutiepii_Robot.modules.helper_funcs.extraction import extract_user
 
 @run_async
 def abuse(bot: Bot, update: Update):
-    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
-    message = update.effective_message
-    if message.reply_to_message:
-      message.reply_to_message.reply_text(random.choice(SFW_STRINGS))
-    else:
-      message.reply_text(random.choice(SFW_STRINGS))
+    msg = update.effective_message
+    reply_text = (
+        msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
+    )
+    reply_text(random.choice(fun_strings.ABUSE_STRINGS))
 
-DARK_HANDLER = DisableAbleCommandHandler("abuse", abuse)
+ABUSE_HANDLER = DisableAbleCommandHandler("abuse", abuse)
 
-dispatcher.add_handler(DARK_HANDLER)
+dispatcher.add_handler(ABUSE_HANDLER)
+
+__handlers__ = [
+    ABUSE_HANDLER
+]
