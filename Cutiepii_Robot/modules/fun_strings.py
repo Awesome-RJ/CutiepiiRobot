656e0c196bf54b7a5ccb2cddf1b99619ebbb2a04
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2021-01-03 11:21:59 +0530

    Update fun_strings.py

--- Cutiepii_Robot/modules/fun_strings.py
+++ Cutiepii_Robot/modules/fun_strings.py
@@ -20,6 +20,37 @@ RUN_STRINGS = (
     "As The Doctor would say... RUN!",
 )
 
+ABUSE_STRINGS = (
+   "What the fuck just happened?/What the fuck is going on here?",
+   "(Respectfully), shut the fuck up!",
+   "They may go fuck themselves",
+   "I’m fucking with you",
+   "Your software can choke on my fucking balls",
+   "I’ll show you fucking vision",
+   "fucking prick",
+   "Oh, for fuck’s sake!",
+   "Are you fucking serious right now?",
+   "Let’s fucking do this!",
+   "fuck off",
+   "cool shit!/shit codebase /What a shit circus!/Oh, shit!",
+   "beat the shit out of someone",
+   "look like shit",
+   "You’re fucking piece of shit!",
+   "Why don’t you choke on my ball?",
+   "cock-sucker",
+   "pussy",
+   "You saved my ass",
+   "I was suck a dick to you",
+   "You are working your ass off",
+   "suck his dick",
+   "dickhole",
+   "Now, that’s just one episode full of curse words.",
+   "son of a bitch or SOB",
+   "backstabbing bitch",
+   "Taking the piss",
+)
+
+
 GIFS = [
     'CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE',
     'CgACAgUAAx0CU_rCTAABAjdgX1s4NVaeCls6YaH3p43vgdCRwQIAAqsAA4P_MFUYQhyoR-kgpRsE',
