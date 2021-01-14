ec440e7a8beb275d9bc80a9f405bb1e26b8e166a
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-01-14 12:23:23 +0530

    Update myanimelist.py

--- Cutiepii_Robot/modules/myanimelist.py
+++ Cutiepii_Robot/modules/myanimelist.py
@@ -6,7 +6,7 @@ from jikanpy.exceptions import APIException
 from telegram import Message, Chat, User, ParseMode, Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 
-from SaitamaRobot import dispatcher
+from Cutiepii_Robot import dispatcher
 
 jikan = Jikan()
 
