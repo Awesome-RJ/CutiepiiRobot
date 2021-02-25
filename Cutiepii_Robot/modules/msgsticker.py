879caa125951dad326ad8c6dbe5a4f6d8ef4d37f
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-02-25 18:46:38 +0530

    Update msgsticker.py

--- Cutiepii_Robot/modules/msgsticker.py
+++ Cutiepii_Robot/modules/msgsticker.py
@@ -12,7 +12,7 @@ import random
 import json
 import os
 import re
-from Cutiepii_Robot.Cutiepii_Robot import Cutiepii_Robot
+from Cutiepii_Robot.cutiepii import Cutiepii_Robot
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
