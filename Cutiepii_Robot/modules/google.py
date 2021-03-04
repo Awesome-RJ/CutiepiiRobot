ac3d52c13d098d7c7062d7ef1d7e308864ffb9ef
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-03-04 12:15:54 +0530

    [batch] Comments and minor edits for batch files

--- Cutiepii_Robot/modules/google.py
+++ Cutiepii_Robot/modules/google.py
@@ -1,22 +1,60 @@
-import subprocess
-from telegram import Update, Bot
-from telegram.ext import run_async, Filters
+  
+import html2text
+import requests
+from telethon import *
+from telethon.tl import functions, types
+from telethon.tl.types import *
 
-from Cutiepii_Robot import dispatcher
-from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
+from Cutiepii_Robot.events import register
 
-def google(bot: Bot, update: Update):
-        query = update.effective_message.text.split(" ", 1)
-        result_ = subprocess.run(['gsearch', str(query[1])], stdout=subprocess.PIPE)
-        result = str(result_.stdout.decode())
-        update.effective_message.reply_markdown('*Searching:*\n`' + str(query[1]) + '`\n\n*RESULTS:*\n' + result)
 
-__help__ = """
- - /google: Google search
- """
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
 
-__mod_name__ = "Google"
+        return isinstance(
+            (
+                await client(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    elif isinstance(chat, types.InputPeerChat):
 
-GOOGLE_HANDLER = DisableAbleCommandHandler("google", google)
+        ui = await client.get_peer_id(user)
+        ps = (
+            await client(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    else:
+        return None
 
-dispatcher.add_handler(GOOGLE_HANDLER)
+
+@register(pattern="^/google (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.is_group:
+        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+            await event.reply(
+                " Hai.. You are not admin..  You can't use this command.. But you can use in my pm"
+            )
+            return
+    # SHOW_DESCRIPTION = False
+    input_str = event.pattern_match.group(
+        1
+    )  # + " -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)"
+    input_url = "https://bots.shrimadhavuk.me/search/?q={}".format(input_str)
+    headers = {"USER-AGENT": "UniBorg"}
+    response = requests.get(input_url, headers=headers).json()
+    output_str = " "
+    for result in response["results"]:
+        text = result.get("title")
+        url = result.get("url")
+        description = result.get("description")
+        last = html2text.html2text(description)
+        output_str += "[{}]({})\n{}\n".format(text, url, last)
+    await event.reply(
+        "{}".format(output_str), link_preview=False, parse_mode="Markdown"
+    )
