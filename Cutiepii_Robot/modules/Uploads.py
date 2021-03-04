fc7d467e421ffeaa03d74c8b5b90de7d14c41c9f
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-03-04 15:45:06 +0530

    Create Uploads.py

---
+++ Cutiepii_Robot/modules/Uploads.py
@@ -0,0 +1,12 @@
+
+__help__ = """
+*Link To File:*
+ • `/up`*:* reply to a direct download link to upload it to telegram as files
+ 
+ *File To Link:*
+ ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link
+ ⦁ `/tmpninja`*:* reply to a telegram file to upload it on tmpninja and get direct download link
+© @Cutiepii_Robot
+"""
+
+__mod_name__ = "Upload"
