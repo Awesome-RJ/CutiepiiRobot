f1faaba2e3030bb00162d300c46678adde410736
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-03-04 16:31:43 +0530

    Update Uploads.py

--- Cutiepii_Robot/modules/Uploads.py
+++ Cutiepii_Robot/modules/Uploads.py
@@ -6,7 +6,6 @@ __help__ = """
  *File To Link:*
  ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link
  ⦁ `/tmpninja`*:* reply to a telegram file to upload it on tmpninja and get direct download link
-© @Cutiepii_Robot
 """
 
 __mod_name__ = "Upload"
