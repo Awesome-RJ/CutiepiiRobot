f4f480715ce0e82806fe8182e9d23f20b85bebe0
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-18 19:51:01 +0530

    Update dbfunc.py

--- Cutiepii_Robot/utils/dbfunc.py
+++ Cutiepii_Robot/utils/dbfunc.py
@@ -2,7 +2,10 @@ from Cutiepii_Robot import db
 from typing import Dict, List, Union
 
 
+
 coupledb = db.couple
+karmadb = db.karma
+
 
 
 # Couple Chooser
@@ -32,6 +35,56 @@ async def save_couple(chat_id: int, date: str, couple: dict):
         {
             "$set": {
                 "couple": lovers
+            }
+        },
+        upsert=True
+    )
+
+
+# Karma functions
+
+
+async def get_karmas_count() -> dict:
+    chats = karmadb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return {}
+    chats_count = 0
+    karmas_count = 0
+    for chat in await chats.to_list(length=1000000):
+        for i in chat['karma']:
+            karmas_count += chat['karma'][i]['karma']
+        chats_count += 1
+    return {
+        "chats_count": chats_count,
+        "karmas_count": karmas_count
+    }
+
+
+async def get_karmas(chat_id: int) -> Dict[str, int]:
+    karma = karmadb.find_one({"chat_id": chat_id})
+    if karma:
+        karma = karma['karma']
+    else:
+        karma = {}
+    return karma
+
+
+async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    karmas = await get_karmas(chat_id)
+    if name in karmas:
+        return karmas[name]
+
+
+async def update_karma(chat_id: int, name: str, karma: dict):
+    name = name.lower().strip()
+    karmas = await get_karmas(chat_id)
+    karmas[name] = karma
+    karmadb.update_one(
+        {"chat_id": chat_id},
+        {
+            "$set": {
+                "karma": karmas
             }
         },
         upsert=True
