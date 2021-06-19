1034b417e4ec4babea5558a45b817729d940c063
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-06-19 18:57:45 +0530

    fucking shit

--- Cutiepii_Robot/modules/stickers.py
+++ Cutiepii_Robot/modules/stickers.py
@@ -13,8 +13,8 @@ from telegram import TelegramError, Update
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from Cutiepii_Robot import dispatcher
+from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
