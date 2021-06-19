a7b245155aa92f7af678629f697bd43eb63c3a2f
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-19 13:06:18 +0530

    Update karma.py

--- Cutiepii_Robot/modules/karma.py
+++ Cutiepii_Robot/modules/karma.py
@@ -111,3 +111,12 @@ async def karma(_, message):
         else:
             karma = 0
             await message.reply_text(f'**Total Points**: __{karma}__')
+
+
+__mod_name__ = "Karma"
+
+__help__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
+[DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
+Reply to a message with /karma to check a user's karma
+Send /karma without replying to any message to chek karma list of top 10 users
+"""
