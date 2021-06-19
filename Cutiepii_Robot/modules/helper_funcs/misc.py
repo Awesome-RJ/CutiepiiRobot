943b7f217b5f5676e424ef402682e1dccadc3bd3
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-06-19 19:47:25 +0530

    Update misc.py

--- Cutiepii_Robot/modules/helper_funcs/misc.py
+++ Cutiepii_Robot/modules/helper_funcs/misc.py
@@ -1,7 +1,7 @@
 from math import ceil
 from typing import Dict, List
 
-from SaitamaRobot import NO_LOAD
+from Cutiepii_Robot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
