f2fff4f872643e51c04d1eb18361b790e21c0073
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-19 13:00:37 +0530

    Update karma.py

--- Cutiepii_Robot/modules/karma.py
+++ Cutiepii_Robot/modules/karma.py
@@ -96,7 +96,7 @@ async def karma(_, message):
             if limit > 9:
                 break
             try:
-                user_name = (await EREN.get_users(int(user_idd))).username
+                user_name = (await cutiepii.get_users(int(user_idd))).username
             except Exception:
                 continue
             msg += f"{user_name} : `{karma_count}`\n"
