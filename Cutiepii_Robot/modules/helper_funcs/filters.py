42441a0a0a43654251e1f4b3963f4885c1562e13
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-06-28 13:56:48 +0530

    Update filters.py

--- Cutiepii_Robot/modules/helper_funcs/filters.py
+++ Cutiepii_Robot/modules/helper_funcs/filters.py
@@ -1,28 +1,28 @@
 from Cutiepii_Robot import DEV_USERS, DRAGONS, DEMONS
 from telegram import Message
-from telegram.ext import MessageFilter
+from telegram.ext import BaseFilter
 
 
 class CustomFilters(object):
-    class _Supporters(MessageFilter):
+    class _Supporters(BaseFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in DEMONS)
 
     support_filter = _Supporters()
 
-    class _Sudoers(MessageFilter):
+    class _Sudoers(BaseFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in DRAGONS)
 
     sudo_filter = _Sudoers()
 
-    class _Developers(MessageFilter):
+    class _Developers(BaseFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in DEV_USERS)
 
     dev_filter = _Developers()
 
-    class _MimeType(MessageFilter):
+    class _MimeType(BaseFilter):
         def __init__(self, mimetype):
             self.mime_type = mimetype
             self.name = "CustomFilters.mime_type({})".format(self.mime_type)
@@ -34,7 +34,7 @@ class CustomFilters(object):
 
     mime_type = _MimeType
 
-    class _HasText(MessageFilter):
+    class _HasText(BaseFilter):
         def filter(self, message: Message):
             return bool(
                 message.text
