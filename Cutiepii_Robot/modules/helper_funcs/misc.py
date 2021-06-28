a2ff9c4d9c656f49e618c5bdfc42534330af1910
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-06-28 12:30:58 +0530

    Update misc.py

--- Cutiepii_Robot/modules/helper_funcs/misc.py
+++ Cutiepii_Robot/modules/helper_funcs/misc.py
@@ -70,11 +70,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("↖️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back", callback_data="cutiepii_back"),
+                EqInlineKeyboardButton("【Ｂａｃｋ】", callback_data="cutiepii_back"),
              EqInlineKeyboardButton("↘️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="cutiepii_back")]]
+        pairs += [[EqInlineKeyboardButton("【Ｂａｃｋ】", callback_data="cutiepii_back")]]
 
 
     return pairs
