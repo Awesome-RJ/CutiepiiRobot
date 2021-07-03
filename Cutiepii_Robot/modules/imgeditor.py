de8efbe09549aa34b31d6d39a3eedc602eb6e044
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+PAINBOI2008@users.noreply.github.com>
2021-07-03 18:21:17 +0530

    Update imgeditor.py

--- Cutiepii_Robot/modules/imgeditor.py
+++ Cutiepii_Robot/modules/imgeditor.py
@@ -453,7 +453,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 __mod_name__ = "Image Editor"
 __help__ = """
  *IMAGE EDITOR*
-Eren has some advanced image editing tools inbuilt
+Cutiepii has some advanced image editing tools inbuilt
 Bright, Circle, RemBG, Blur, Border, Flip, Glitch, Sticker maker and more
 
 - /edit [reply to image]: Open the image editor
