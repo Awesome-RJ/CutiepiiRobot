cadb8369bb9afe15a4d98545a7a8b7b087b22b37
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-07-07 12:32:06 +0530

    Fixed

--- Cutiepii_Robot/modules/mongo/couples_mongo.py
+++ Cutiepii_Robot/modules/mongo/couples_mongo.py
@@ -1,14 +1,17 @@
-from Cutiepii_Robot import db
+from CutiepiiRobot import db
 from typing import Dict, List, Union
-
 
 coupledb = db.couple
 
+# Couple Chooser
+
 async def _get_lovers(chat_id: int):
-    lovers = await coupledb.find_one({"chat_id": chat_id})
-    if not lovers:
-        return {}
-    return lovers["couple"]
+    lovers = coupledb.find_one({"chat_id": chat_id})
+    if lovers:
+        lovers = lovers["couple"]
+    else:
+        lovers = {}
+    return lovers
 
 
 async def get_couple(chat_id: int, date: str):
@@ -22,6 +25,33 @@ async def get_couple(chat_id: int, date: str):
 async def save_couple(chat_id: int, date: str, couple: dict):
     lovers = await _get_lovers(chat_id)
     lovers[date] = couple
-    await coupledb.update_one(
-        {"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True
+    coupledb.update_one(
+        {"chat_id": chat_id},
+        {
+            "$set": {
+                "couple": lovers
+            }
+        },
+        upsert=True
     )
+
+
+# Alpha integer 
+
+async def int_to_alpha(user_id: int) -> str:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    text = ""
+    user_id = str(user_id)
+    for i in user_id:
+        text += alphabet[int(i)]
+    return text
+
+
+async def alpha_to_int(user_id_alphabet: str) -> int:
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    user_id = ""
+    for i in user_id_alphabet:
+        index = alphabet.index(i)
+        user_id += str(index)
+    user_id = int(user_id)
+    return user_id
