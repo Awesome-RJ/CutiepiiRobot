437baec9e11fbd3fdb77922e33fd222f27296084
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-07-18 18:25:24 +0530

    Updated ARQ

--- Cutiepii_Robot/__init__.py
+++ Cutiepii_Robot/__init__.py
@@ -3,20 +3,17 @@ import os
 import sys
 import time
 import spamwatch
-from pyrogram import Client, errors
-import telegram.ext as tg
-from telethon import TelegramClient
-from telethon.sessions import StringSession
 from motor import motor_asyncio
 from odmantic import AIOEngine
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from redis import StrictRedis
-from Python_ARQ import ARQ
-import aiohttp
+
+import telegram.ext as tg
+from pyrogram import Client, errors
+from telethon import TelegramClient
 from aiohttp import ClientSession
-from ptbcontrib.postgres_persistence import PostgresPersistence
-from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
+from Python_ARQ import ARQ
 
 StartTime = time.time()
 
@@ -96,7 +93,8 @@ if ENV:
     MONGO_PORT = int(os.environ.get("MONGO_PORT", None))
     MONGO_DB = os.environ.get("MONGO_DB", None)
     REDIS_URL = os.environ.get("REDIS_URI", None)
-    ARQ_API = os.environ.get("ARQ_API", None)
+    ARQ_API_URL = "https://thearq.tech"
+    ARQ_API_KEY = "YIECCC-NAJARO-OLLREW-SJSRIP-ARQ"
     BOT_ID = int(os.environ.get("BOT_ID", None))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
@@ -216,8 +214,19 @@ finally:
 
 if not SPAMWATCH_API:
     sw = None
-    LOGGER.warning("[CUTIEPII ERROR]: SpamWatch API key Is Missing! Recheck Your Config.")
+           LOGGER.warning("Can't connect to SpamWatch!")
 else:
+
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+telethn = TelegramClient("cutiepii", API_ID, API_HASH)
+pgram = Client("cutiepiipgram", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+print("[INFO]: INITIALIZING AIOHTTP SESSION")
+aiohttpsession = ClientSession()
+# ARQ Client
+print("[INFO]: INITIALIZING ARQ CLIENT")
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+dispatcher = updater.dispatcher
+
     try:
         sw = spamwatch.Client(SPAMWATCH_API)
     except:
@@ -244,10 +253,7 @@ motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = motor[MONGO_DB]
 engine = AIOEngine(motor, MONGO_DB)
 print("[INFO]: INITIALZING AIOHTTP SESSION")
-aiohttpsession = ClientSession()
-# ARQ Client
 print("[INFO]: INITIALIZING ARQ CLIENT")
-arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 print("[CUTIEPII]: Connecting To Yūki • Data Center • Mumbai • PostgreSQL Database")
 ubot = TelegramClient(StringSession(STRING_SESSION), APP_ID, APP_HASH)
 print("[CUTIEPII]: Connecting To Yūki • Cutiepii Userbot (t.me/Awesome_Cutiepii)")
