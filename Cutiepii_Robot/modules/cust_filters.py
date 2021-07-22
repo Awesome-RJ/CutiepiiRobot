d12fcd3027ea99c37b771555c1e83e2c312408ab
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-07-22 18:48:15 +0530

    Update cust_filters.py

--- Cutiepii_Robot/modules/cust_filters.py
+++ Cutiepii_Robot/modules/cust_filters.py
@@ -617,6 +617,24 @@ doin?
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
 Check `/markdownhelp` to know more!
 
+*Auto Filters*
+Cutiepii Can filter content of a given channel automatically
+Currently support:
+- Videos
+- Media
+- Documents
+- Music
+
+*Setting up*
+1) Add @Cutiepii_Robot to your channel
+2) Make bot admin with full permissions
+2) Go back to your group
+
+*Commands*
+ • `/add [Channel Username | Channel ID]`*:* Add given channel to autofiltering
+ • `/del [Channel Username | Channel ID]`*:* Remove given channel from auto filtering
+ • `/delall`*:* Remove all channels from automatic filtering
+ • `/autofiltersettings`*:* Show settings panel about auto filtering channels
 """
 
 __mod_name__ = "Filters"
