636d5804f6a070f8a20a6c874dab65f61a8d5b1c
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-07-22 17:04:34 +0530

    Update auto_filters.py

--- Cutiepii_Robot/modules/auto_filters.py
+++ Cutiepii_Robot/modules/auto_filters.py
@@ -1,5 +1,6 @@
 __help__ = """
-*AUTO FILTERS*
+
+**AUTO FILTERS**
 Cutiepii Can filter content of a given channel automatically
 Currently support:
      - Videos
@@ -7,16 +8,18 @@ Currently support:
      - Documents
      - Music
 
-*Setting up*
+**Setting up**
 1) Add @Cutiepii_Robot to your channel
 2) Make bot admin with full permissions
 2) Go back to your group
 
-*Commands*
+**Commands**
  • `/add [Channel Username | Channel ID]`*:* Add given channel to autofiltering
  • `/del [Channel Username | Channel ID]`*:* Remove given channel from auto filtering
  • `/delall`*:* Remove all channels from automatic filtering
  • `/autofiltersettings`*:* Show settings panel about auto filtering channels
+ 
 """
 
 __mod_name__ = "Auto Filters"
+
