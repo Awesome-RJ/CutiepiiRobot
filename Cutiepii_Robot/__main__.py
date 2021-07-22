8c2ecc8d47af6a8810dee0ff82a20b3088878755
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-07-22 21:17:35 +0530

    Update __main__.py

--- Cutiepii_Robot/__main__.py
+++ Cutiepii_Robot/__main__.py
@@ -724,5 +724,5 @@ except BaseException:
 if __name__ == '__main__':
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
-    pbot.start()
+    pgram.start()
     idle()
