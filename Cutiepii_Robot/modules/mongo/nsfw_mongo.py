85fbf50b8360bfc553c5ecde04769daf46ca4957
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-28 20:41:20 +0700

    Update nsfw_mongo.py

--- Cutiepii_Robot/modules/mongo/nsfw_mongo.py
+++ Cutiepii_Robot/modules/mongo/nsfw_mongo.py
@@ -19,7 +19,7 @@ async def nsfw_on(chat_id: int):
 
 
 async def nsfw_off(chat_id: int):
-    is_nsfw = is_nsfw_on(chat_id)
+    is_nsfw = await is_nsfw_on(chat_id)
     if not is_nsfw:
         return
     return nsfwdb.insert_one({"chat_id": chat_id})
