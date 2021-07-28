5adcf1e10a1c3d505d4f9b0e9840b1ed4f77de18
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-07-28 17:40:31 +0530

    `DisableAbleMessageHandler` Missed

--- Cutiepii_Robot/modules/fun.py
+++ Cutiepii_Robot/modules/fun.py
@@ -11,7 +11,7 @@ import Cutiepii_Robot.modules.fun_strings as fun_strings
 from pyrogram import filters
 from pathlib import Path
 from Cutiepii_Robot import DEMONS, DRAGONS, pgram as bot, dispatcher
-from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
+from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
 from Cutiepii_Robot.modules.helper_funcs.chat_status import is_user_admin
 from Cutiepii_Robot.modules.helper_funcs.extraction import extract_user
 from telegram import ChatPermissions, ParseMode, Update
