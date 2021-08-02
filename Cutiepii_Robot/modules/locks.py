234c648fb9ef6630bbebd10dccdef38f02e4c3dc
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-08-03 01:35:19 +0530

    consider using safer `ast.literal_eval`

--- Cutiepii_Robot/modules/locks.py
+++ Cutiepii_Robot/modules/locks.py
@@ -271,7 +271,7 @@ def lock(update, context) -> str:
                 context.bot.set_chat_permissions(
                     chat_id=chat_id,
                     permissions=get_permission_list(
-                        eval(str(current_permission)),
+                        ast.literal_eval(str(current_permission)),
                         LOCK_CHAT_RESTRICTION[ltype.lower()],
                     ),
                 )
@@ -373,7 +373,7 @@ def unlock(update, context) -> str:
                 context.bot.set_chat_permissions(
                     chat_id=chat_id,
                     permissions=get_permission_list(
-                        eval(str(current_permission)),
+                        ast.literal_eval(str(current_permission)),
                         UNLOCK_CHAT_RESTRICTION[ltype.lower()],
                     ),
                 )
