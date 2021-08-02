3d8042b0711705067bea27d94d0f257ec0b5dafa
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-08-02 18:26:51 +0530

    Update kuki_sql.py

--- Cutiepii_Robot/modules/sql/kuki_sql.py
+++ Cutiepii_Robot/modules/sql/kuki_sql.py
@@ -1,43 +1,42 @@
 import threading
-
 from sqlalchemy import Column, String
 from Cutiepii_Robot.modules.sql import BASE, SESSION
-class CutiepiiChats(BASE):
-    __tablename__ = "cutiepii_chats"
+class KukiChats(BASE):
+    __tablename__ = "kuki_chats"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
-CutiepiiChats.__table__.create(checkfirst=True)
+KukiChats.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def is_cutiepii(chat_id):
+def is_kuki(chat_id):
     try:
-        chat = SESSION.query(CutiepiiChats).get(str(chat_id))
+        chat = SESSION.query(KukiChats).get(str(chat_id))
         return bool(chat)
     finally:
         SESSION.close()
 
-def set_cutiepii(chat_id):
+def set_kuki(chat_id):
     with INSERTION_LOCK:
-        cutiepiichat = SESSION.query(CutiepiiChats).get(str(chat_id))
-        if not cutiepiichat:
-            cutiepiichat = CutiepiiChats(str(chat_id))
-        SESSION.add(cutiepiichat)
+        kukichat = SESSION.query(KukiChats).get(str(chat_id))
+        if not kukichat:
+            kukichat = KukiChats(str(chat_id))
+        SESSION.add(kukichat)
         SESSION.commit()
 
-def rem_cutiepii(chat_id):
+def rem_kuki(chat_id):
     with INSERTION_LOCK:
-        cutiepiichat = SESSION.query(CutiepiiChats).get(str(chat_id))
-        if cutiepiichat:
-            SESSION.delete(cutiepiichat)
+        kukichat = SESSION.query(KukiChats).get(str(chat_id))
+        if kukichat:
+            SESSION.delete(kukichat)
         SESSION.commit()
 
 
-def get_all_cutiepii_chats():
+def get_all_kuki_chats():
     try:
-        return SESSION.query(CutiepiiChats.chat_id).all()
+        return SESSION.query(KukiChats.chat_id).all()
     finally:
         SESSION.close()
