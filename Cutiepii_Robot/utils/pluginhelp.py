3c2d8b9e6e62517886145e169b68cc110be13d1f
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-08-03 00:44:33 +0530

    `range(len(time_list))` TO `enumerate(time_list)`

--- Cutiepii_Robot/utils/pluginhelp.py
+++ Cutiepii_Robot/utils/pluginhelp.py
@@ -53,7 +53,7 @@ def get_readable_time(seconds: int) -> int:
         time_list.append(int(result))
         seconds = int(remainder)
 
-    for x in range(len(time_list)):
+    for x in enumerate(time_list):
         time_list[x] = str(time_list[x]) + time_suffix_list[x]
     if len(time_list) == 4:
         ping_time += time_list.pop() + ", "
