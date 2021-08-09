ee61f6a3b7f377563bfe11be9396bea14dce605d
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-08-09 15:27:18 +0530

    beta

--- Cutiepii_Robot/modules/shippering.py
+++ Cutiepii_Robot/modules/shippering.py
@@ -1,39 +1,36 @@
 import random
 
-from datetime import datetime
-from pyrogram import filters
-from Cutiepii_Robot import pgram as app, BOT_USERNAME
+from Cutiepii_Robot import pgram as app
 from Cutiepii_Robot.utils.errors import capture_err
 from Cutiepii_Robot.modules.mongo.couples_mongo import get_couple, save_couple
+from pyrogram import filters
+from datetime import datetime
 
-__mod_name__ = "Shippering"
 
 # Date and time
 def dt():
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M")
-    return dt_string.split(" ")
+    dt_list = dt_string.split(' ')
+    return dt_list
 
 
 def dt_tom():
-    return (
-        str(int(dt()[0].split("/")[0]) + 1)
-        + "/"
-        + dt()[0].split("/")[1]
-        + "/"
-        + dt()[0].split("/")[2]
-    )
+    a = str(int(dt()[0].split('/')[0]) + 1)+"/" + \
+        dt()[0].split('/')[1]+"/" + dt()[0].split('/')[2]
+    return a
 
 
 today = str(dt()[0])
 tomorrow = str(dt_tom())
 
 
-@app.on_message(filters.command("couples", f"couples@{BOT_USERNAME}") & ~filters.edited)
+@app.on_message(filters.command("couples") & ~filters.edited)
 @capture_err
 async def couple(_, message):
     if message.chat.type == "private":
-        return await message.reply_text("This command only works in groups.")
+        await message.reply_text("This command only works in groups.")
+        return
     try:
         chat_id = message.chat.id
         is_selected = await get_couple(chat_id, today)
@@ -43,7 +40,8 @@ async def couple(_, message):
                 if not i.user.is_bot:
                     list_of_users.append(i.user.id)
             if len(list_of_users) < 2:
-                return await message.reply_text("Not enough users")
+                await message.reply_text("Not enough users")
+                return
             c1_id = random.choice(list_of_users)
             c2_id = random.choice(list_of_users)
             while c1_id == c2_id:
@@ -55,22 +53,30 @@ async def couple(_, message):
 {c1_mention} + {c2_mention} = ❤️
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
             await app.send_message(
-                message.chat.id, text=couple_selection_message
+                message.chat.id,
+                text=couple_selection_message
             )
-            couple = {"c1_id": c1_id, "c2_id": c2_id}
+            couple = {
+                "c1_id": c1_id,
+                "c2_id": c2_id
+            }
             await save_couple(chat_id, today, couple)
 
-        else:
-            c1_id = int(is_selected["c1_id"])
-            c2_id = int(is_selected["c2_id"])
+        elif is_selected:
+            c1_id = int(is_selected['c1_id'])
+            c2_id = int(is_selected['c2_id'])
             c1_name = (await app.get_users(c1_id)).first_name
             c2_name = (await app.get_users(c2_id)).first_name
             couple_selection_message = f"""Couple of the day:
 [{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
             await app.send_message(
-                message.chat.id, text=couple_selection_message
+                message.chat.id,
+                text=couple_selection_message
             )
     except Exception as e:
         print(e)
-        await message.reply_text(e)
+        message.reply_text(e)
+
+
+__mod_name__ = "Couples"
