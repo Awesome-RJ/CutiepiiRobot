32533bf22f960e110f3213b93953d7b14c3764b9
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2021-08-12 04:44:40 +0530

    Vers Done

--- Cutiepii_Robot/__init__.py
+++ Cutiepii_Robot/__init__.py
@@ -74,48 +74,48 @@ if ENV:
     except ValueError:
         raise Exception("Your scout users list does not contain valid integers.")
 
-    INFOPIC = bool(os.environ.get("INFOPIC", False))
-    EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
+    INFOPIC = bool(os.environ.get("INFOPIC", False)) # Info Pic (use True[Value] If You Want To Show In /info.)
+    EVENT_LOGS = os.environ.get("EVENT_LOGS", None) # G-Ban Logs (Channel) (-100)
+    ERROR_LOGS = os.environ.get("ERROR_LOGS", None) # Error Logs (Channel Ya Group Choice Is Yours) (-100)
     WEBHOOK = bool(os.environ.get("WEBHOOK", False))
-    URL = os.environ.get("URL", "")  # Does not contain token
-    PORT = int(os.environ.get("PORT", 5000))
+    URL = os.environ.get("URL", "")  # If You Deploy On Heraku. [URL PERTEN:- https://{App Name}.herokuapp.com/ || EXP:- https://yuki-cutiepii-robot.herokuapp.com/]
+    PORT = int(os.environ.get("PORT", 5000)) 
     CERT_PATH = os.environ.get("CERT_PATH")
-    API_ID = os.environ.get("API_ID", None)
-    API_HASH = os.environ.get("API_HASH", None)
-    DB_URI = os.environ.get("DATABASE_URL")
-    DONATION_LINK = os.environ.get("DONATION_LINK")
-    LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
-    WORKERS = int(os.environ.get("WORKERS", 8))
-    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
-    WALL_API = os.environ.get("WALL_API", None)
-    MONGO_URI = os.environ.get("MONGO_DB_URI", None)
-    MONGO_PORT = int(os.environ.get("MONGO_PORT", None))
-    MONGO_DB = os.environ.get("MONGO_DB", None)
-    REDIS_URL = os.environ.get("REDIS_URI", None)
-    BOT_ID = int(os.environ.get("BOT_ID", None))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
-    SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
-    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
-    ARQ_API_URL = "https://thearq.tech"
-    ARQ_API_KEY = "YIECCC-NAJARO-OLLREW-SJSRIP-ARQ"
-    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", "")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
-    APP_ID = os.environ.get("APP_ID", None)
-    APP_HASH = os.environ.get("APP_HASH", None)
-    GENIUS_API_TOKEN = os.environ.get("GENIUS_API_TOKEN", None)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", True)
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", True)
-    ERROR_LOGS = os.environ.get("ERROR_LOGS", None)
-    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
+    API_ID = os.environ.get("API_ID", None) # Bot Owner's API_ID (From:- https://my.telegram.org/auth)
+    API_HASH = os.environ.get("API_HASH", None) # Bot Owner's API_HASH (From:- https://my.telegram.org/auth)
+    DB_URL = os.environ.get("DATABASE_URL") # Any SQL Database Link (RECOMMENDED:- PostgreSQL & https://www.elephantsql.com)
+    DONATION_LINK = os.environ.get("DONATION_LINK") # Donation Link (ANY)
+    LOAD = os.environ.get("LOAD", "").split() # Don't Change
+    NO_LOAD = os.environ.get("NO_LOAD", "translation").split() # Don't Change
+    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False)) # Don't Change
+    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False)) # Use `True` Value
+    WORKERS = int(os.environ.get("WORKERS", 8)) # Don't Change
+    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg") # Don't Change
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False) # Don't Change
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./") # Don't Change
+    CASH_API_KEY = os.environ.get("CASH_API_KEY", None) # From:- https://www.alphavantage.co/support/#api-key
+    TIME_API_KEY = os.environ.get("TIME_API_KEY", None) # From:- https://timezonedb.com/api
+    WALL_API = os.environ.get("WALL_API", None) # From:- https://wall.alphacoders.com/api.php
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None) # From:- https://www.remove.bg/
+    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", "") # From:- https://openweathermap.org/api
+    GENIUS_API_TOKEN = os.environ.get("GENIUS_API_TOKEN", None) # From:- http://genius.com/api-clients
+    MONGO_DB_URL = os.environ.get("MONGO_DB_URL", None) # MongoDB URL (From:- https://www.mongodb.com/)
+    MONGO_PORT = int(os.environ.get("MONGO_PORT", None)) # MongoDB Port (RECOMMENDED PORT:- 27017)
+    MONGO_DB = os.environ.get("MONGO_DB", None) # Any Type Of Name (EXP:- cutiepii)
+    REDIS_URL = os.environ.get("REDIS_URL", None) # REDIS URL (From:- Heraku & Redis)
+    BOT_ID = int(os.environ.get("BOT_ID", None)) # Telegram Bot ID (EXP:- 1241223850)
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None) # Support Chat Group Link (Use @Black_Knights_Union_Support || Dont Use https://t.me/Black_Knights_Union_Support)
+    SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None) # Use @SpamWatchSupport
+    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None) # From https://t.me/SpamWatchBot 
+    ARQ_API_URL = "https://thearq.tech" # Don't Change
+    ARQ_API_KEY = "YIECCC-NAJARO-OLLREW-SJSRIP-ARQ" # From https://t.me/ARQRobot
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # Bot Username
+    STRING_SESSION = os.environ.get("STRING_SESSION", None) # Telethon Based String Session (2nd ID) [ From https://repl.it/@SpEcHiDe/GenerateStringSession ]
+    APP_ID = os.environ.get("APP_ID", None) # 2nd ID 
+    APP_HASH = os.environ.get("APP_HASH", None) # 2nd ID
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", True) # Heroku App Name 
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", True) # Heroku API [From https://dashboard.heroku.com/account]
+    ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True) # Don't Change
 
     try:
         BL_CHATS = {int(x) for x in os.environ.get("BL_CHATS", "").split()}
@@ -156,39 +156,43 @@ else:
     except ValueError:
         raise Exception("Your scout users list does not contain valid integers.")
 
-    EVENT_LOGS = Config.EVENT_LOGS
+    INFOPIC = Config.INFOPIC
+    EVENT_LOGS = Config.EVENT_LOGS 
+    ERROR_LOGS = Config.ERROR_LOGS
     WEBHOOK = Config.WEBHOOK
     URL = Config.URL
     PORT = Config.PORT
     CERT_PATH = Config.CERT_PATH
     API_ID = Config.API_ID
     API_HASH = Config.API_HASH
-
-    DB_URI = Config.DATABASE_URL
+    DB_URL = Config.DB_URL
     DONATION_LINK = Config.DONATION_LINK
-    LOAD = Config.LOAD
-    NO_LOAD = Config.NO_LOAD
-    DEL_CMDS = Config.DEL_CMDS
     STRICT_GBAN = Config.STRICT_GBAN
     WORKERS = Config.WORKERS
     BAN_STICKER = Config.BAN_STICKER
-    ALLOW_EXCL = Config.ALLOW_EXCL
+    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
     CASH_API_KEY = Config.CASH_API_KEY
     TIME_API_KEY = Config.TIME_API_KEY
-    REDIS_URL = Config.REDIS_URL
     WALL_API = Config.WALL_API
+    MONGO_DB_URL = Config.MONGO_DB_URL
+    MONGO_PORT = Config.MONGO_PORT
+    MONGO_DB = Config.MONGO_DB
+    REDIS_URL = Config.REDIS_URL
     SUPPORT_CHAT = Config.SUPPORT_CHAT
     SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
     SPAMWATCH_API = Config.SPAMWATCH_API
-    INFOPIC = Config.INFOPIC
+    ARQ_API_URL = Config.ARQ_API_URL
+    ARQ_API_KEY = Config.ARQ_API_KEY
+    REM_BG_API_KEY = Config.REM_BG_API_KEY
+    OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
     APP_ID = Config.APP_ID
     APP_HASH = Config.APP_HASH
-    MONGO_URI = Config.MONGO_DB_URI
-    MONGO_PORT = Config.MONGO_PORT
-    MONGO_DB = Config.MONGO_DB
-    ARQ_API = Config.ARQ_API
     BOT_ID = Config.BOT_ID
-    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
+    BOT_USERNAME = Config.BOT_USERNAME
+    STRING_SESSION = Config.STRING_SESSION
+    GENIUS_API_TOKEN = Config.GENIUS_API_TOKEN
+    HEROKU_APP_NAME = Config.HEROKU_APP_NAME
+    HEROKU_API_KEY = Config.HEROKU_API_KEY
 
     try:
         BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
@@ -249,8 +253,8 @@ pgram = Client(
     bot_token=TOKEN,
 )
 print("[CUTIEPII]: Connecting To Yūki • Data Center • Mumbai • MongoDB Database")
-mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+mongodb = MongoClient(MONGO_DB_URL, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URL)
 db = motor[MONGO_DB]
 engine = AIOEngine(motor, MONGO_DB)
 print("[INFO]: INITIALZING AIOHTTP SESSION")
--- Cutiepii_Robot/modules/sql/__init__.py
+++ Cutiepii_Robot/modules/sql/__init__.py
@@ -1,11 +1,11 @@
-from Cutiepii_Robot import DB_URI
+from Cutiepii_Robot import DB_URL
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(DB_URL, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
