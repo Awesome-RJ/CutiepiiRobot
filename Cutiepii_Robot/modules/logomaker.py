099cb8325121b19d9d1746d497df806758d51f19
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2021-09-13 17:06:14 +0530

    test

--- Cutiepii_Robot/modules/logomaker.py
+++ Cutiepii_Robot/modules/logomaker.py
@@ -1,82 +1,83 @@
-import os 
+import os
+import random
 
 from PIL import Image, ImageDraw, ImageFont
 
 from Cutiepii_Robot.events import register
-from Cutiepii_Robot import OWNER_ID, telethn, BOT_USERNAME, SUPPORT_CHAT
+from Cutiepii_Robot import OWNER_ID, telethn
+
+
+logopics = [
+ 
+ "./Cutiepii_Robot/utils/Logo/90681adbbef50d4fba1d6cc261d62cd9.jpg",
+	
+ "./Cutiepii_Robot/utils/Logo/73b69af0502031831c904fc536118ce1.jpg",
+	
+ "./Cutiepii_Robot/utils/Logo/6aaf0d7f5db3dce7595b27635645efc6.jpg",
+	
+ "./Cutiepii_Robot/utils/Logo/73b69af0502031831c904fc536118ce1.jpg"
+
+ "./Cutiepii_Robot/utils/Logo/90681adbbef50d4fba1d6cc261d62cd9.jpg",
+	
+ "./Cutiepii_Robot/utils/Logo/ebd7de41ff63c46ad9742b53f3a16d7b.jpg",
+	
+ ".hblackbg.jpg",
+ 
+]
+
+logofonts = [
+ 
+ "./Cutiepii_Robot/utils/Logo/RemachineScriptPersonalUseOnly-yZL3.ttf",
+	
+ "./Cutiepii_Robot/utils/Logo/beyond-wonderland.regular.ttf",
+	
+ "./Cutiepii_Robot/utils/Logo/Respective-VP6y.ttf"
+
+ "./Cutiepii_Robot/utils/Logo/Maghrib.ttf",
+	
+ "./Cutiepii_Robot/utils/Logo/Chopsic.otf",
+ 
+]
 
 
 @register(pattern="^/logo ?(.*)")
 async def lego(event):
  quew = event.pattern_match.group(1)
- if event.sender_id != OWNER_ID and not quew:
-  await event.reply('Provide Some Text To Draw!')
-  return
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
+
+    if not quew:
+       await event.reply('Provide Some Text To Draw!')
+       return
+    else:
+       pass
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./Cutiepii_Robot/utils/Logo/blackbg.jpg')
+    img = Image.open(random.choice(logopics))
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./Cutiepii_Robot/utils/Logo/Chopsic.otf", 330)
+    font = ImageFont.truetype(random.choice(logofonts) , 250)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
-    y = ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByCutiepii.png"
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="yellow", stroke_width=25, stroke_fill="black")
+    fname2 = "Cutiepii_Logo.png"
     img.save(fname2, "png")
-    await telethn.send_file(event.chat_id, fname2, caption=f"Made By @{BOT_USERNAME}")
+    await telethn.send_file(event.chat_id, fname2, caption = f"**Made By @{BOT_USERNAME}**")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @{SUPPORT_CHAT}, {e}')
-
-
+   await event.reply(f'**Error Report @{SUPPORT_CHAT}**, {e}')
 
    
-@register(pattern="^/wlogo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id != OWNER_ID and not quew:
-  await event.reply('Provide Some Text To Draw!')
-  return
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./Cutiepii_Robot/utils/Logo/blackbg.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./Cutiepii_Robot/utils/Logo/Maghrib.ttf", 1000)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y = ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByCutiepii.png"
-    img.save(fname2, "png")
-    await telethn.send_file(event.chat_id, fname2, caption=f"Made By @{BOT_USERNAME}")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error Report @{SUPPORT_CHAT}, {e}')
-
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
-
-
-__help__ = """
- ❍ /logo text :  Create your logo with your name
- """
-__mod_name__ = "Logo"
