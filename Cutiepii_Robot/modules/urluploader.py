39ee5010969f63f23aa68c213af9c47411896d4b
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2021-09-13 15:58:21 +0530

    fixed

--- Cutiepii_Robot/modules/urluploader.py
+++ Cutiepii_Robot/modules/urluploader.py
@@ -29,7 +29,7 @@ async def download_coroutine(session, url, file_name, event, start, bot):
 **URL:** {}
 **File Name:** {}
 **File Size:** {}
-**© @{BOT_USERNAME}**""".format(
+**© @Cutiepii_Robot**""".format(
                 url,
                 os.path.basename(file_name).replace("%20", " "),
                 humanbytes(total_length),
