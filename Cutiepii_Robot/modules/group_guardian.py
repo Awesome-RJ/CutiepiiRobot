331d1ce4639b5daf2b51f632b67bfa2a88400706
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2021-09-17 23:04:15 +0530

    | Yūki • Black Knights Union Project | PTB:- 13.7

---
+++ Cutiepii_Robot/modules/group_guardian.py
@@ -0,0 +1,211 @@
+
+import asyncio
+import os
+import re
+import better_profanity
+import emoji
+import nude
+import requests
+
+from better_profanity import profanity
+from google_trans_new import google_translator
+from telethon import events
+from telethon.tl.types import ChatBannedRights
+
+# from DaisyX.db.mongo_helpers.nsfw_guard import add_chat, get_all_nsfw_chats, is_chat_in_db, rm_chat
+from Cutiepii_Robot.utils.pluginhelpers import telethn, BOT_ID, is_admin, mongodb as db
+from Cutiepii_Robot..events import register
+
+translator = google_translator()
+MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
+
+
+# This Module is ported from https://github.com/MissJuliaRobot/MissJuliaRobot
+# This hardwork was completely done by MissJuliaRobot
+# Full Credits goes to MissJuliaRobot
+
+
+approved_users = db.approve
+spammers = db.spammer
+globalchat = db.globchat
+
+CMD_STARTERS = "/"
+profanity.load_censor_words_from_file("./profanity_wordlist.txt")
+
+
+@register(pattern="^/profanity(?: |$)(.*)")
+async def profanity(event):
+    if event.fwd_from:
+        return
+    if not event.is_group:
+        await event.reply("You Can Only profanity in Groups.")
+        return
+    event.pattern_match.group(1)
+    if not await is_admin(event, BOT_ID):
+        await event.reply("`I Should Be Admin To Do This!`")
+        return
+    if await is_admin(event, event.message.sender_id):
+        input = event.pattern_match.group(1)
+        chats = spammers.find({})
+        if not input:
+            for c in chats:
+                if event.chat_id == c["id"]:
+                    await event.reply(
+                        "Please provide some input yes or no.\n\nCurrent setting is : **on**"
+                    )
+                    return
+            await event.reply(
+                "Please provide some input yes or no.\n\nCurrent setting is : **off**"
+            )
+            return
+        if input == "on":
+            if event.is_group:
+                chats = spammers.find({})
+                for c in chats:
+                    if event.chat_id == c["id"]:
+                        await event.reply(
+                            "Profanity filter is already activated for this chat."
+                        )
+                        return
+                spammers.insert_one({"id": event.chat_id})
+                await event.reply("Profanity filter turned on for this chat.")
+        if input == "off":
+            if event.is_group:
+                chats = spammers.find({})
+                for c in chats:
+                    if event.chat_id == c["id"]:
+                        spammers.delete_one({"id": event.chat_id})
+                        await event.reply("Profanity filter turned off for this chat.")
+                        return
+            await event.reply("Profanity filter isn't turned on for this chat.")
+        if not input == "on" and not input == "off":
+            await event.reply("I only understand by on or off")
+            return
+    else:
+        await event.reply("`You Should Be Admin To Do This!`")
+        return
+
+
+@register(pattern="^/globalmode(?: |$)(.*)")
+async def profanity(event):
+    if event.fwd_from:
+        return
+    if not event.is_group:
+        await event.reply("You Can Only enable global mode Watch in Groups.")
+        return
+    event.pattern_match.group(1)
+    if not await is_admin(event, BOT_ID):
+        await event.reply("`I Should Be Admin To Do This!`")
+        return
+    if await is_admin(event, event.message.sender_id):
+
+        input = event.pattern_match.group(1)
+        chats = globalchat.find({})
+        if not input:
+            for c in chats:
+                if event.chat_id == c["id"]:
+                    await event.reply(
+                        "Please provide some input yes or no.\n\nCurrent setting is : **on**"
+                    )
+                    return
+            await event.reply(
+                "Please provide some input yes or no.\n\nCurrent setting is : **off**"
+            )
+            return
+        if input == "on":
+            if event.is_group:
+                chats = globalchat.find({})
+                for c in chats:
+                    if event.chat_id == c["id"]:
+                        await event.reply(
+                            "Global mode is already activated for this chat."
+                        )
+                        return
+                globalchat.insert_one({"id": event.chat_id})
+                await event.reply("Global mode turned on for this chat.")
+        if input == "off":
+            if event.is_group:
+                chats = globalchat.find({})
+                for c in chats:
+                    if event.chat_id == c["id"]:
+                        globalchat.delete_one({"id": event.chat_id})
+                        await event.reply("Global mode turned off for this chat.")
+                        return
+            await event.reply("Global mode isn't turned on for this chat.")
+        if not input == "on" and not input == "off":
+            await event.reply("I only understand by on or off")
+            return
+    else:
+        await event.reply("`You Should Be Admin To Do This!`")
+        return
+
+
+def extract_emojis(s):
+    return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
+
+
+@telethn.on(events.NewMessage(pattern=None))
+async def del_profanity(event):
+    if event.is_private:
+        return
+    msg = str(event.text)
+    sender = await event.get_sender()
+    # sender.username
+    if await is_admin(event, event.message.sender_id):
+        return
+    chats = globalchat.find({})
+    for c in chats:
+        if event.text:
+            if event.chat_id == c["id"]:
+                u = msg.split()
+                emj = extract_emojis(msg)
+                msg = msg.replace(emj, "")
+                if (
+                    [(k) for k in u if k.startswith("@")]
+                    and [(k) for k in u if k.startswith("#")]
+                    and [(k) for k in u if k.startswith("/")]
+                    and re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []
+                ):
+                    h = " ".join(filter(lambda x: x[0] != "@", u))
+                    km = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", h)
+                    tm = km.split()
+                    jm = " ".join(filter(lambda x: x[0] != "#", tm))
+                    hm = jm.split()
+                    rm = " ".join(filter(lambda x: x[0] != "/", hm))
+                elif [(k) for k in u if k.startswith("@")]:
+                    rm = " ".join(filter(lambda x: x[0] != "@", u))
+                elif [(k) for k in u if k.startswith("#")]:
+                    rm = " ".join(filter(lambda x: x[0] != "#", u))
+                elif [(k) for k in u if k.startswith("/")]:
+                    rm = " ".join(filter(lambda x: x[0] != "/", u))
+                elif re.findall(r"\[([^]]+)]\(\s*([^)]+)\s*\)", msg) != []:
+                    rm = re.sub(r"\[([^]]+)]\(\s*([^)]+)\s*\)", r"", msg)
+                else:
+                    rm = msg
+                # print (rm)
+                b = translator.detect(rm)
+                if not "en" in b and not b == "":
+                    await event.delete()
+                    st = sender.first_name
+                    hh = sender.id
+                    final = f"[{st}](tg://user?id={hh}) you should only speak in english here !"
+                    dev = await event.respond(final)
+                    await asyncio.sleep(10)
+                    await dev.delete()
+
+
+#
+
+__help__ = """
+*Group Guardian*:
+✪ Cutiepii can protect your group from NSFW senders, Slag word users and also can force members to use English
+
+Commmands
+  ➢ `/nsfwguardian on/off`*:* Enable|Disable Porn cleaning
+  ➢ `/globalmode on/off`*:* Enable|Disable English only mode
+  ➢ `/profanity on/off`*:* Enable|Disable slag word cleaning
+ 
+Note: Special credits goes to Julia project and Friday Userbot
+ 
+"""
+__mod_name__ = "Group Guardian"
