6f4d9951a8727808547b65df029995c03b585500
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-18 13:41:26 +0530

    Update nhentai.py

--- Cutiepii_Robot/modules/nhentai.py
+++ Cutiepii_Robot/modules/nhentai.py
@@ -1,26 +1,74 @@
-import Helper.formating_results as format
+import requests
 
-from telethon import events
+from pyrogram import filters
+from pyrogram.types import (InlineKeyboardMarkup,
+                            InlineKeyboardButton,
+                            InlineQueryResultArticle,
+                            InputTextMessageContent
+                            )
 
-from Cutiepii_Robot.utils.nhentaiapi import Nhentaiapi as nh
-from Cutiepii_Robot import telethn
+from Cutiepii_Robot import pgram, telegraph
 
-class Nhentai():
+from Cutiepii_Robot.utils.errors import capture_err
 
-    @telethn.on(events.NewMessage(pattern="/nh"))
-    async def event_handler_anime(event):
-        if '/nh' == event.raw_text:
-            await bot.send_message(
-                event.chat_id,
-                'Command must be used like this\n/nh <hentai code\nexample: /nh 848264'
-            )
-        elif '/nh' in event.raw_text:
-            text = event.raw_text.split()
-            text.pop(0)
-            code = " ".join(text)
-            chapter = nh.get_chapter_by_code(code)
-            format.manga_chapter_html(f"{code}", chapter)
-            await bot.send_message(
-                event.chat_id,
-                "Open this in google chrome",
-                file= f"{code}.html"
+
+@pgram.on_message(~filters.me & filters.command('nhentai', prefixes='/'), group=8)
+@capture_err
+async def nhentai(client, message):
+    query = message.text.split(" ")[1]
+    title, tags, artist, total_pages, post_url, cover_image = nhentai_data(query)
+    await message.reply_text(
+        f"<code>{title}</code>\n\n<b>Tags:</b>\n{tags}\n<b>Artists:</b>\n{artist}\n<b>Pages:</b>\n{total_pages}",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Read Here",
+                        url=post_url
+                    )
+                ]
+            ]
+        )
+    )
+
+
+def nhentai_data(noombers):
+    url = f"https://nhentai.net/api/gallery/{noombers}"
+    res = requests.get(url).json()
+    pages = res["images"]["pages"]
+    info = res["tags"]
+    title = res["title"]["english"]
+    links = []
+    tags = ""
+    artist = ''
+    total_pages = res['num_pages']
+    extensions = {
+        'j':'jpg',
+        'p':'png',
+        'g':'gif'
+    }
+    for i, x in enumerate(pages):
+        media_id = res["media_id"]
+        temp = x['t']
+        file = f"{i+1}.{extensions[temp]}"
+        link = f"https://i.nhentai.net/galleries/{media_id}/{file}"
+        links.append(link)
+
+    for i in info:
+        if i["type"]=="tag":
+            tag = i['name']
+            tag = tag.split(" ")
+            tag = "_".join(tag)
+            tags+=f"#{tag} "
+        if i["type"]=="artist":
+            artist=f"{i['name']} "
+
+    post_content = "".join(f"<img src={link}><br>" for link in links)
+
+    post = telegraph.create_page(
+        f"{title}",
+        html_content=post_content,
+        author_name="@Cutiepii_Robot", 
+        author_url="https://t.me/Cutiepii_Robot"
+    )
+    return title,tags,artist,total_pages,post['url'],links[0]
