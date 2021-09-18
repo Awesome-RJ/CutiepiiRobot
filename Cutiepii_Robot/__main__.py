34b2c5a2e175cbdd54968fafcb80bec93082588c
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-18 11:47:58 +0530

    fuck

--- Cutiepii_Robot/__main__.py
+++ Cutiepii_Robot/__main__.py
@@ -239,7 +239,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(
-                    escape_markdown(context.bot.first_name)
+                    escape_markdown(context.bot.first_name),
                     escape_markdown(first_name),
                     escape_markdown(uptime),
                     sql.num_users(),
