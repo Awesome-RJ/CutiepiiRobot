ed6ac942bd5ca6b6085224f999c254300e60d641
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-24 14:39:00 +0530

    test

--- Cutiepii_Robot/modules/reverse.py
+++ Cutiepii_Robot/modules/reverse.py
@@ -11,14 +11,14 @@ from bs4 import BeautifulSoup
 from telegram import Update, InputMediaPhoto, TelegramError
 from telegram.ext import CallbackContext
 
-from Cutiepii_Robot import dispatcher
-from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
+from Cutiepii_Robot.modules.helper_funcs.decorators import cutiepiicmd
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36"
 opener.addheaders = [("User-agent", useragent)]
 
 
+@cutiepiicmd(command=["grs", "reverse"], pass_args=True)
 def reverse(update: Update, context: CallbackContext):
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
@@ -202,10 +202,3 @@ def scam(imgspage, lim):
             break
 
     return imglinks
-
-
-REVERSE_HANDLER = DisableAbleCommandHandler(
-    ["grs", "reverse"], reverse, pass_args=True, run_async=True
-)
-
-dispatcher.add_handler(REVERSE_HANDLER)
