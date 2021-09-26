a4c1c6eb02ed51ef0321b547adbadb784026c2c4
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-26 17:30:25 +0530

    No need

--- Cutiepii_Robot/modules/userinfo.py
+++ Cutiepii_Robot/modules/userinfo.py
@@ -62,30 +62,6 @@ def get_percentage(totalhp, earnedhp):
     return per_of_totalhp
 
 
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x, _ in enumerate(time_list):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if time_list == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
-
 
 def hpmanager(user):
     total_hp = (get_user_num_chats(user.id) + 10) * 10
