e25c881567b17182f7de71803b329c0c9f34d9c5
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-26 18:57:21 +0530

    revert

--- Cutiepii_Robot/modules/cust_filters.py
+++ Cutiepii_Robot/modules/cust_filters.py
@@ -399,9 +399,10 @@ def reply_filter(update, context):
                     ENUM_FUNC_MAP[filt.file_type](
                         chat.id,
                         filt.file_id,
-                        caption=filtext,
+                        caption=markdown_to_html(filtext),
                         reply_to_message_id=message.message_id,
                         parse_mode=ParseMode.HTML,
+                        disable_web_page_preview=True,
                         reply_markup=keyboard,
                     )
                 break
