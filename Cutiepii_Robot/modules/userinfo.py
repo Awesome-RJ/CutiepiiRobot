05e06e7e763ec7d9f66a5239c15f1030f8522751
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-26 19:44:25 +0530

    `get_readable_time` readded

--- Cutiepii_Robot/modules/userinfo.py
+++ Cutiepii_Robot/modules/userinfo.py
@@ -60,7 +60,29 @@ def get_percentage(totalhp, earnedhp):
     per_of_totalhp = str(int(per_of_totalhp))
     return per_of_totalhp
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
 
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
 
 def hpmanager(user):
     total_hp = (get_user_num_chats(user.id) + 10) * 10
