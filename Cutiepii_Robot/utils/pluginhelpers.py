0e6008cdbc5bcf9fbf3172fdaa884f1f55d7a83d
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-02 16:19:31 +0530

    `is_admin` fixed

--- Cutiepii_Robot/utils/pluginhelpers.py
+++ Cutiepii_Robot/utils/pluginhelpers.py
@@ -36,7 +36,10 @@ def get_user(message: Message, text: str) -> [int, str, None]:
 async def is_admin(event, user):
     try:
         sed = await event.client.get_permissions(event.chat_id, user)
-        is_mod = bool(sed.is_admin)
+        if sed.is_admin:
+            is_mod = True
+        else:
+            is_mod = False
     except:
         is_mod = False
     return is_mod
