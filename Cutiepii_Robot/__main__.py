2984d15c21e09611f1ceac653141e1349cc4e66e
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-02 18:27:15 +0530

    Update __main__.py

--- Cutiepii_Robot/__main__.py
+++ Cutiepii_Robot/__main__.py
@@ -698,7 +698,7 @@ def main():
 
     if WEBHOOK:
         LOGGER.info("Using webhooks.")
-        updater.start_webhook(listen="127.0.0.1", port=PORT, url_path=TOKEN)
+        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 
         if CERT_PATH:
             updater.bot.set_webhook(url=URL + TOKEN, certificate=open(CERT_PATH, "rb"))
