b2bfc8b2cae5eb96bd15f90c3542efa3b1efbf34
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-10-05 13:55:26 +0530

    Update lewd.py

--- Cutiepii_Robot/modules/lewd.py
+++ Cutiepii_Robot/modules/lewd.py
@@ -22,7 +22,7 @@ def add_nsfw(update: Update, context: CallbackContext):
     chat = update.effective_chat
     msg = update.effective_message
     user = update.effective_user #Remodified by @EverythingSuckz
-    is_nsfw = sql.is_nsfw(chat.id)
+    is_nsfw = sql.set_nsfw(chat.id)
     if not is_nsfw:
         sql.set_nsfw(chat.id)
         msg.reply_text("Activated NSFW Mode!")
@@ -42,7 +42,7 @@ def rem_nsfw(update: Update, context: CallbackContext):
     msg = update.effective_message
     chat = update.effective_chat
     user = update.effective_user
-    is_nsfw = sql.is_nsfw(chat.id)
+    is_nsfw = sql.rem_nsfw(chat.id)
     if not is_nsfw:
         msg.reply_text("NSFW Mode is already Deactivated")
         return ""
