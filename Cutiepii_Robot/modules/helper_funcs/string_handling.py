2636f6e7de055586537ddd8b6405cbeb10d47663
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-06 21:52:15 +0530

    improved

--- Cutiepii_Robot/modules/helper_funcs/string_handling.py
+++ Cutiepii_Robot/modules/helper_funcs/string_handling.py
@@ -86,32 +86,26 @@ def markdown_parser(
         end = ent.offset + offset + ent.length - 1  # end of entity
 
         # we only care about code, url, text links
-        if ent.type in ("code", "url", "text_link"):
-            # count emoji to switch counter
-            count = _calc_emoji_offset(txt[:start])
-            start -= count
-            end -= count
+        if ent.type not in ("code", "url", "text_link"):
+            continue
 
-            # URL handling -> do not escape if in [](), escape otherwise.
-            if ent.type == "url":
-                if any(
-                    match.start(1) <= start and end <= match.end(1)
-                    for match in LINK_REGEX.finditer(txt)
-                ):
-                    continue
+        # count emoji to switch counter
+        count = _calc_emoji_offset(txt[:start])
+        start -= count
+        end -= count
+
+        # URL handling -> do not escape if in [](), escape otherwise.
+        if ent.type == "url":
+            if any(
+                match.start(1) <= start and end <= match.end(1)
+                for match in LINK_REGEX.finditer(txt)
+            ):
+                continue
+            # else, check the escapes between the prev and last and forcefully escape the url to avoid mangling
+            else:
                 # TODO: investigate possible offset bug when lots of emoji are present
                 res += _selective_escape(txt[prev:start] or "") + escape_markdown(
-                    ent_text,
-                )
-
-            # code handling
-            elif ent.type == "code":
-                res += _selective_escape(txt[prev:start]) + "`" + ent_text + "`"
-
-            # handle markdown/html links
-            elif ent.type == "text_link":
-                res += _selective_escape(txt[prev:start]) + "[{}]({})".format(
-                    ent_text, ent.url,
+                    ent_text
                 )
 
             end += 1
