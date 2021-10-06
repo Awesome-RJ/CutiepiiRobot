00109e292c64e35800e7c975c71f3755a26b9a63
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-06 21:41:39 +0530

    `Leave Chat Button Added

--- Cutiepii_Robot/modules/dev.py
+++ Cutiepii_Robot/modules/dev.py
@@ -1,3 +1,4 @@
+import re
 import os
 import subprocess
 import sys
@@ -7,11 +8,24 @@ from time import sleep
 
 import Cutiepii_Robot
 
-from Cutiepii_Robot import dispatcher
+from Cutiepii_Robot import dispatcher, DEV_USERS
 from Cutiepii_Robot.modules.helper_funcs.chat_status import dev_plus
-from telegram import TelegramError, Update
+from telegram.ext.callbackqueryhandler import CallbackQueryHandler
+from telegram import TelegramError, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, run_async
+
+def leave_cb(update: Update, context: CallbackContext):
+    bot = context.bot
+    callback = update.callback_query
+    if callback.from_user.id not in DEV_USERS:
+        callback.answer(text="This isn't for you", show_alert=True)
+        return
+
+    match = re.match(r"leavechat_cb_\((.+?)\)", callback.data)
+    chat = int(match.group(1))
+    bot.leave_chat(chat_id=chat)
+    callback.answer(text="Left chat")
 
 @dev_plus
 def allow_groups(update: Update, context: CallbackContext):
@@ -35,7 +49,9 @@ def leave(update: Update, context: CallbackContext):
     args = context.args
     if args:
         chat_id = str(args[0])
+	leave_msg = " ".join(args[1:])
         try:
+	    context.bot.send_message(chat_id, leave_msg)
             bot.leave_chat(int(chat_id))
         except TelegramError:
             update.effective_message.reply_text(
@@ -45,8 +61,13 @@ def leave(update: Update, context: CallbackContext):
         with suppress(Unauthorized):
             update.effective_message.reply_text("Beep boop, I left that soup!.")
     else:
-        update.effective_message.reply_text("Send a valid chat ID")
-
+        chat = update.effective_chat
+        # user = update.effective_user
+        cutiepii_leave_bt = [[
+            InlineKeyboardButton(text="I am sure of this action.", callback_data="leavechat_cb_({})".format(chat.id))
+        ]]
+        update.effective_message.reply_text("I'm going to leave {}, press the button below to confirm".format(chat.title), reply_markup=InlineKeyboardMarkup(cutiepii_leave_bt))
+	
 
 @dev_plus
 def gitpull(update: Update, context: CallbackContext):
@@ -79,11 +100,13 @@ LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
 GITPULL_HANDLER = CommandHandler("gitpull", gitpull, run_async=True)
 RESTART_HANDLER = CommandHandler("reboot", restart, run_async=True)
 ALLOWGROUPS_HANDLER = CommandHandler("lockdown", allow_groups, run_async=True)
+LEAVE_CALLBACK_HANDLER = CallbackQueryHandler(leave_cb, pattern=r"leavechat_cb_", run_async=True)
 
 dispatcher.add_handler(ALLOWGROUPS_HANDLER)
 dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
+dispatcher.add_handler(LEAVE_CALLBACK_HANDLER)
 
 __mod_name__ = "Dev"
-__handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
+__handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER, LEAVE_CALLBACK_HANDLER]
