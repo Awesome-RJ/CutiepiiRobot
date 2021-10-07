7e20493c6e1684da6cca3a22f8048dfd341afd33
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-07 15:26:44 +0530

    Update string_handling.py

--- Cutiepii_Robot/modules/helper_funcs/string_handling.py
+++ Cutiepii_Robot/modules/helper_funcs/string_handling.py
@@ -168,29 +168,33 @@ def escape_invalid_curly_brackets(text: str, valids: List[str]) -> str:
                 idx += 2
                 new_text += "{{{{"
                 continue
+            else:
                 success = False
                 for v in valids:
                     if text[idx:].startswith("{" + v + "}"):
                         success = True
                         break
-            if success:
-                new_text += text[idx : idx + len(v) + 2]
-                idx += len(v) + 2
-                continue
-            new_text += "{{"
+                if success:
+                    new_text += text[idx : idx + len(v) + 2]
+                    idx += len(v) + 2
+                    continue
+                else:
+                    new_text += "{{"
 
         elif text[idx] == "}":
             if idx + 1 < len(text) and text[idx + 1] == "}":
                 idx += 2
                 new_text += "}}}}"
                 continue
-            new_text += "}}"
+            else:
+                new_text += "}}"
 
         else:
             new_text += text[idx]
         idx += 1
 
     return new_text
+
 
 
 SMART_OPEN = "“"
