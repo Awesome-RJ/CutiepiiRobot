42eda5e829ff5c09fd3e54dc32c58f9a3fdf7824
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-07 13:33:45 +0530

    `if` statements can be merged

--- Cutiepii_Robot/modules/zip.py
+++ Cutiepii_Robot/modules/zip.py
@@ -44,13 +44,14 @@ async def _(event):
     if not event.is_reply:
         await event.reply("Reply to a file to compress it.")
         return
-    if event.is_group:
-        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-            await event.reply(
-                "Hey, You are not admin. You can't use this command, But you can use in my pm 🙂"
-            )
-            return
-
+    if (
+        event.is_group
+        and not (await is_register_admin(event.input_chat, event.message.sender_id))
+    ):
+        await event.reply(
+            "Hey, You are not admin. You can't use this command, But you can use in my pm 🙂"
+        )
+        return
     mone = await event.reply("⏳️ Please wait...")
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
