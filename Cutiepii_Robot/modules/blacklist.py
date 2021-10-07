ba83ecd43f3b23a4e3de887a8ce2e0fde0f2075b
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-07 13:46:14 +0530

    Consider using `in`

--- Cutiepii_Robot/modules/blacklist.py
+++ Cutiepii_Robot/modules/blacklist.py
@@ -221,11 +221,10 @@ def blacklist_mode(update, context):
         chat_name = update.effective_message.chat.title
 
     if args:
-        if (args[0].lower() == "off" or args[0].lower() == "nothing" or
-                args[0].lower() == "no"):
+        if args[0].lower() in ("off", "nothing", "no"):
             settypeblacklist = "do nothing"
             sql.set_blacklist_strength(chat_id, 0, "0")
-        elif args[0].lower() == "del" or args[0].lower() == "delete":
+        elif args[0].lower() in ("del", "delete"):
             settypeblacklist = "will delete blacklisted message"
             sql.set_blacklist_strength(chat_id, 1, "0")
         elif args[0].lower() == "warn":
