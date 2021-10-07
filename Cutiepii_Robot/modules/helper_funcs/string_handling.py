f8c2129449658dc99ff88d08ad6320919ccb136a
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-07 14:13:37 +0530

    trying to improving

--- Cutiepii_Robot/modules/helper_funcs/string_handling.py
+++ Cutiepii_Robot/modules/helper_funcs/string_handling.py
@@ -86,40 +86,32 @@ def markdown_parser(
         end = ent.offset + offset + ent.length - 1  # end of entity
 
         # we only care about code, url, text links
-        if ent.type not in ("code", "url", "text_link"):
-            continue
+        if ent.type in ("code", "url", "text_link"):
+            # count emoji to switch counter
+            count = _calc_emoji_offset(txt[:start])
+            start -= count
+            end -= count
 
-        # count emoji to switch counter
-        count = _calc_emoji_offset(txt[:start])
-        start -= count
-        end -= count
-
-        # URL handling -> do not escape if in [](), escape otherwise.
-        if ent.type == "url":
-            if any(
-                match.start(1) <= start and end <= match.end(1)
-                for match in LINK_REGEX.finditer(txt)
-            ):
-                continue
-            # else, check the escapes between the prev and last and forcefully escape the url to avoid mangling
-            else:
-                # TODO: investigate possible offset bug when lots of emoji are present
-                res += _selective_escape(txt[prev:start] or "") + escape_markdown(
-                    ent_text
-                )
-
-        # code handling
-        elif ent.type == "code":
-            res += _selective_escape(txt[prev:start]) + "`" + ent_text + "`"
-            
-        # handle markdown/html links
-        elif ent.type == "text_link":
-            res += _selective_escape(txt[prev:start]) + "[{}]({})".format(
-                ent_text, ent.url
-            )
+            # URL handling -> do not escape if in [](), escape otherwise.
+            if ent.type == "url":
+                if any(
+                    match.start(1) <= start and end <= match.end(1)
+                    for match in LINK_REGEX.finditer(txt)
+                ):
+                    continue
+                # else, check the escapes between the prev and last and forcefully escape the url to avoid mangling
+                else:
+                    # TODO: investigate possible offset bug when lots of emoji are present
+                    res += _selective_escape(txt[prev:start] or "") + escape_markdown(
+                        ent_text
+                    )
 
         end += 1
-
+        
+        # anything else
+        else:
+            continue
+            
         prev = end
 
     res += _selective_escape(txt[prev:])  # add the rest of the text
@@ -166,11 +158,11 @@ def escape_invalid_curly_brackets(text: str, valids: List[str]) -> str:
                 idx += 2
                 new_text += "{{{{"
                 continue
-            success = False
-            for v in valids:
-                if text[idx:].startswith("{" + v + "}"):
-                    success = True
-                    break
+                success = False
+                for v in valids:
+                    if text[idx:].startswith("{" + v + "}"):
+                        success = True
+                        break
             if success:
                 new_text += text[idx : idx + len(v) + 2]
                 idx += len(v) + 2
@@ -197,33 +189,35 @@ START_CHAR = ("'", '"', SMART_OPEN)
 
 
 def split_quotes(text: str) -> List:
-    if not any(text.startswith(char) for char in START_CHAR):
-        return text.split(None, 1)
-    counter = 1  # ignore first char -> is some kind of quote
-    while counter < len(text):
-        if text[counter] == "\\":
+    if any(text.startswith(char) for char in START_CHAR):
+        counter = 1  # ignore first char -> is some kind of quote
+        while counter < len(text):
+            if text[counter] == "\\":
+                counter += 1
+            elif text[counter] == text[0] or (
+                text[0] == SMART_OPEN and text[counter] == SMART_CLOSE
+            ):
+                break
             counter += 1
-        elif text[counter] == text[0] or (
-            text[0] == SMART_OPEN and text[counter] == SMART_CLOSE
-        ):
-            break
-        counter += 1
+        else:
+            return text.split(None, 1)
+
+        # 1 to avoid starting quote, and counter is exclusive so avoids ending
+        key = remove_escapes(text[1:counter].strip())
+        # index will be in range, or `else` would have been executed and returned
+        rest = text[counter + 1 :].strip()
+        if not key:
+            key = text[0] + text[0]
+        return list(filter(None, [key, rest]))
     else:
         return text.split(None, 1)
 
-    # 1 to avoid starting quote, and counter is exclusive so avoids ending
-    key = remove_escapes(text[1:counter].strip())
-    # index will be in range, or `else` would have been executed and returned
-    rest = text[counter + 1 :].strip()
-    if not key:
-        key = text[0] + text[0]
-    return list(filter(None, [key, rest]))
-
 
 def remove_escapes(text: str) -> str:
+    counter = 0
     res = ""
     is_escaped = False
-    for counter in range(len(text)):
+    while counter < len(text):
         if is_escaped:
             res += text[counter]
             is_escaped = False
@@ -231,6 +225,7 @@ def remove_escapes(text: str) -> str:
             is_escaped = True
         else:
             res += text[counter]
+        counter += 1
     return res
 
 
