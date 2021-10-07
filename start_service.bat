c667fc1ffa271c66c9bb803f745679dd7b05e46c
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-07 12:42:47 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop Cutiepii_Robot
-net start Cutiepii_Robot
+
+nssm restart Cutiepii_Robot
