8bed8802f4ae179a46b98051d2a05c8e9321a10c
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-12 19:51:56 +0530

    `upvote` error fixed

--- Cutiepii_Robot/modules/karma.py
+++ Cutiepii_Robot/modules/karma.py
@@ -68,7 +68,7 @@ regex_downvote = r"^(\-|\-\-|\-1|👎)$"
     & filters.group
     & filters.incoming
     & filters.reply
-    & filters.regex(regex_upvote)
+    & filters.regex(regex_upvote, re.IGNORECASE)
     & ~filters.via_bot
     & ~filters.bot
     & ~filters.edited,
@@ -78,23 +78,29 @@ regex_downvote = r"^(\-|\-\-|\-1|👎)$"
 async def upvote(_, message):
     if not await is_karma_on(message.chat.id):
         return
+    if not message.reply_to_message.from_user:
+        return
+    if not message.from_user:
+        return
     if message.reply_to_message.from_user.id == message.from_user.id:
         return
     chat_id = message.chat.id
     user_id = message.reply_to_message.from_user.id
     user_mention = message.reply_to_message.from_user.mention
-    current_karma = await get_karma(chat_id, await int_to_alpha(user_id))
+    current_karma = await get_karma(
+        chat_id, await int_to_alpha(user_id)
+    )
     if current_karma:
         current_karma = current_karma['karma']
         karma = current_karma + 1
-        new_karma = {"karma": karma}
-        await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
     else:
         karma = 1
-        new_karma = {"karma": karma}
-        await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
+    new_karma = {"karma": karma}
+    await update_karma(
+        chat_id, await int_to_alpha(user_id), new_karma
+    )
     await message.reply_text(
-        f'Incremented Karma of {user_mention} By 1 \nTotal Points: {karma}'
+        f"Incremented Karma of {user_mention} By 1 \nTotal Points: {karma}"
     )
 
 
