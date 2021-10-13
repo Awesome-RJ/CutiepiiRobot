f35bec2129cc9651e75f0b79b4d5df7a9e31cb3c
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-13 16:37:25 +0530

    Update string_handling.py

--- Cutiepii_Robot/modules/helper_funcs/string_handling.py
+++ Cutiepii_Robot/modules/helper_funcs/string_handling.py
@@ -132,11 +132,17 @@ def markdown_parser(
                         ent_text
                     )
 
-            end += 1
+            # code handling
+            elif ent.type == "code":
+                res += _selective_escape(txt[prev:start]) + "`" + ent_text + "`"
 
-        # anything else
-        else:
-            continue
+            # handle markdown/html links
+            elif ent.type == "text_link":
+                res += _selective_escape(txt[prev:start]) + "[{}]({})".format(
+                    ent_text, ent.url
+                )
+
+            end += 1
 
         # anything else
         else:
