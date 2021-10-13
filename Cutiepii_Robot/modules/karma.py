cc86d195f1536e5e7492d4bfb40932fbcdf3ff46
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-13 15:57:24 +0530

    revrt

--- Cutiepii_Robot/modules/karma.py
+++ Cutiepii_Robot/modules/karma.py
@@ -25,44 +25,23 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import asyncio 
 import re
+
 from pyrogram import filters
+
 from Cutiepii_Robot import pgram, BOT_USERNAME
 from Cutiepii_Robot.utils.errors import capture_err
-from Cutiepii_Robot.modules.mongo.karma_mongo import (update_karma, get_karma, get_karmas,
-                                   int_to_alpha, alpha_to_int, is_karma_on,
-                                   karma_on, karma_off)
+from Cutiepii_Robot.utils.permissions import adminsOnly
+from Cutiepii_Robot.modules.mongo.karma_mongo import (alpha_to_int, get_karma,
+                                   get_karmas, int_to_alpha, update_karma)   
+from Cutiepii_Robot.modules.mongo.karma_mongo import is_karma_on, karma_off, karma_on
 
 karma_positive_group = 3
 karma_negative_group = 4
 
-async def member_permissions(chat_id, user_id):
-    perms = []
-    member = (await pgram.get_chat_member(chat_id, user_id))
-    if member.can_post_messages:
-        perms.append("can_post_messages")
-    if member.can_edit_messages:
-        perms.append("can_edit_messages")
-    if member.can_delete_messages:
-        perms.append("can_delete_messages")
-    if member.can_restrict_members:
-        perms.append("can_restrict_members")
-    if member.can_promote_members:
-        perms.append("can_promote_members")
-    if member.can_change_info:
-        perms.append("can_change_info")
-    if member.can_invite_users:
-        perms.append("can_invite_users")
-    if member.can_pin_messages:
-        perms.append("can_pin_messages")
-    if member.can_manage_voice_chats:
-        perms.append("can_manage_voice_chats")
-    return perms
-
-
 regex_upvote = r"^((?i)\+|\+\+|\+1|thx|tnx|ty|thank you|thanx|thanks|pro|cool|good|👍)$"
 regex_downvote = r"^(\-|\-\-|\-1|👎)$"
-
 
 @pgram.on_message(
     filters.text
@@ -73,7 +52,7 @@ regex_downvote = r"^(\-|\-\-|\-1|👎)$"
     & ~filters.via_bot
     & ~filters.bot
     & ~filters.edited,
-    group=karma_positive_group
+    group=karma_positive_group,
 )
 @capture_err
 async def upvote(_, message):
@@ -110,37 +89,46 @@ async def upvote(_, message):
     & filters.group
     & filters.incoming
     & filters.reply
-    & filters.regex(regex_downvote)
+    & filters.regex(regex_downvote, re.IGNORECASE)
     & ~filters.via_bot
     & ~filters.bot
     & ~filters.edited,
-    group=karma_negative_group
+    group=karma_negative_group,
 )
 @capture_err
 async def downvote(_, message):
     if not await is_karma_on(message.chat.id):
         return
+    if not message.reply_to_message.from_user:
+        return
+    if not message.from_user:
+        return
     if message.reply_to_message.from_user.id == message.from_user.id:
         return
+
     chat_id = message.chat.id
     user_id = message.reply_to_message.from_user.id
     user_mention = message.reply_to_message.from_user.mention
-    current_karma = await get_karma(chat_id, await int_to_alpha(user_id))
+    current_karma = await get_karma(
+        chat_id, await int_to_alpha(user_id)
+    )
     if current_karma:
         current_karma = current_karma['karma']
         karma = current_karma - 1
     else:
         karma = 1
     new_karma = {"karma": karma}
-    await update_karma(chat_id, await int_to_alpha(user_id), new_karma)
+    await update_karma(
+        chat_id, await int_to_alpha(user_id), new_karma
+    )
     await message.reply_text(
-        f'Decremented Karma Of {user_mention} By 1 \nTotal Points: {karma}'
+        f"Decremented Karma Of {user_mention} By 1 \nTotal Points: {karma}"
     )
 
 
-@pgram.on_message(filters.command("karma") & filters.group)
+@pgram.on_message(filters.command("karma", f"karma@{BOT_USERNAME}") & filters.group)
 @capture_err
-async def karma(_, message):
+async def command_karma(_, message):
     chat_id = message.chat.id
     if not message.reply_to_message:
         m = await message.reply_text(
@@ -186,29 +174,24 @@ async def karma(_, message):
         user_id = message.reply_to_message.from_user.id
         karma = await get_karma(chat_id, await int_to_alpha(user_id))
         karma = karma['karma'] if karma else 0
-        await message.reply_text(f'**Total Points**: __{karma}__')
+        await message.reply_text(f"**Total Points**: __{karma}__")
 
 
-@pgram.on_message(filters.command("karmastat") & ~filters.private)
+@pgram.on_message(filters.command("karmas") & ~filters.private)
+@adminsOnly("can_change_info")
 async def captcha_state(_, message):
-    usage = "**Usage:** karmastat ON or OFF "
+    usage = "**Usage:**\n/karmas [on|off]"
     if len(message.command) != 2:
-        await message.reply_text(usage)
-        return
-    user_id = message.from_user.id
+        return await message.reply_text(usage)
     chat_id = message.chat.id
-    permissions = await member_permissions(chat_id, user_id)
-    if "can_restrict_members" not in permissions:
-        await message.reply_text("You don't have enough permissions.")
-        return
     state = message.text.split(None, 1)[1].strip()
     state = state.lower()
     if state == "on":
         await karma_on(chat_id)
-        await message.reply_text("Enabled karma system.")
+        await message.reply_text("Enabled karma System!")
     elif state == "off":
         await karma_off(chat_id)
-        await message.reply_text("Disabled karma system.")
+        await message.reply_text("Disabled karma System!")
     else:
         await message.reply_text(usage)
 
