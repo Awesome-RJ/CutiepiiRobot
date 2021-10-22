72c36bac9de0221633b97b9e167b4b3c56c617eb
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-22 21:10:37 +0530

    extract_unt_fedban improved

--- Cutiepii_Robot/modules/helper_funcs/extraction.py
+++ Cutiepii_Robot/modules/helper_funcs/extraction.py
@@ -124,9 +124,8 @@ def extract_text(message) -> str:
     )
 
 
-def extract_unt_fedban(
-    message: Message, args: List[str],
-) -> (Optional[int], Optional[str]):
+def extract_unt_fedban(message: Message,
+                       args: List[str]) -> tuple(Optional[int], Optional[str]):
     prev_message = message.reply_to_message
     split_text = message.text.split(None, 1)
 
@@ -138,25 +137,33 @@ def extract_unt_fedban(
     text = ""
 
     entities = list(message.parse_entities([MessageEntity.TEXT_MENTION]))
-    ent = entities[0] if entities else None
+    if len(entities) > 0:
+        ent = entities[0]
+    else:
+        ent = None
+
     # if entity offset matches (command end/text start) then all good
-    if entities and ent and ent.offset == len(message.text) - len(text_to_parse):
+    if entities and ent and ent.offset == len(
+            message.text) - len(text_to_parse):
         ent = entities[0]
         user_id = ent.user.id
-        text = message.text[ent.offset + ent.length :]
+        text = message.text[ent.offset + ent.length:]
 
-    elif len(args) >= 1 and args[0][0] == "@":
+    elif len(args) >= 1 and args[0][0] == '@':
         user = args[0]
         user_id = get_user_id(user)
         if not user_id and not isinstance(user_id, int):
             message.reply_text(
                 "I don't have that user in my db.  "
-                "You'll be able to interact with them if you reply to that person's message instead, or forward one of that user's messages.",
+                "You'll be able to interact with them if you reply to that person's message instead, or forward one of that user's messages."
             )
             return None, None
-        res = message.text.split(None, 2)
-        if len(res) >= 3:
-            text = res[2]
+
+        else:
+            user_id = user_id
+            res = message.text.split(None, 2)
+            if len(res) >= 3:
+                text = res[2]
 
     elif len(args) >= 1 and args[0].isdigit():
         user_id = int(args[0])
@@ -173,19 +180,18 @@ def extract_unt_fedban(
     try:
         message.bot.get_chat(user_id)
     except BadRequest as excp:
-        if excp.message in ("User_id_invalid", "Chat not found") and not isinstance(
-            user_id, int,
-        ):
+        if excp.message in ("User_id_invalid",
+                            "Chat not found") and not isinstance(user_id, int):
             message.reply_text(
                 "I don't seem to have interacted with this user before "
                 "please forward a message from them to give me control! "
-                "(like a voodoo doll, I need a piece of them to be able to execute certain commands...)",
+                "(like a voodoo doll, I need a piece of them to be able to execute certain commands...)"
             )
             return None, None
-        if excp.message != "Chat not found":
+        elif excp.message != "Chat not found":
             LOGGER.exception("Exception %s on user %s", excp.message, user_id)
             return None, None
-        if not isinstance(user_id, int):
+        elif not isinstance(user_id, int):
             return None, None
 
     return user_id, text
