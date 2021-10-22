cd4dbb5f4aac15b2445984ee89391bf34b1b1d0e
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-22 21:09:31 +0530

    extract_user_and_text improved

--- Cutiepii_Robot/modules/helper_funcs/extraction.py
+++ Cutiepii_Robot/modules/helper_funcs/extraction.py
@@ -47,9 +47,8 @@ def extract_user(message: Message, args: List[str]) -> Optional[int]:
     return extract_user_and_text(message, args)[0]
 
 
-def extract_user_and_text(
-    message: Message, args: List[str],
-) -> (Optional[int], Optional[str]):
+def extract_user_and_text(message: Message,
+                          args: List[str]) -> tuple(Optional[int], Optional[str]):
     prev_message = message.reply_to_message
     split_text = message.text.split(None, 1)
 
@@ -61,25 +60,33 @@ def extract_user_and_text(
     text = ""
 
     entities = list(message.parse_entities([MessageEntity.TEXT_MENTION]))
-    ent = entities[0] if entities else None
+    if len(entities) > 0:
+        ent = entities[0]
+    else:
+        ent = None
+
     # if entity offset matches (command end/text start) then all good
-    if entities and ent and ent.offset == len(message.text) - len(text_to_parse):
+    if entities and ent and ent.offset == len(
+            message.text) - len(text_to_parse):
         ent = entities[0]
         user_id = ent.user.id
-        text = message.text[ent.offset + ent.length :]
+        text = message.text[ent.offset + ent.length:]
 
-    elif len(args) >= 1 and args[0][0] == "@":
+    elif len(args) >= 1 and args[0][0] == '@':
         user = args[0]
         user_id = get_user_id(user)
         if not user_id:
             message.reply_text(
                 "No idea who this user is. You'll be able to interact with them if "
-                "you reply to that person's message instead, or forward one of that user's messages.",
+                "you reply to that person's message instead, or forward one of that user's messages."
             )
             return None, None
-        res = message.text.split(None, 2)
-        if len(res) >= 3:
-            text = res[2]
+
+        else:
+            user_id = user_id
+            res = message.text.split(None, 2)
+            if len(res) >= 3:
+                text = res[2]
 
     elif len(args) >= 1 and args[0].isdigit():
         user_id = int(args[0])
@@ -100,8 +107,7 @@ def extract_user_and_text(
             message.reply_text(
                 "I don't seem to have interacted with this user before - please forward a message from "
                 "them to give me control! (like a voodoo doll, I need a piece of them to be able "
-                "to execute certain commands...)",
-            )
+                "to execute certain commands...)")
         else:
             LOGGER.exception("Exception %s on user %s", excp.message, user_id)
 
