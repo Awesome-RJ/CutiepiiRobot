796637297b16b7f6db52597a9a4fb086d256d10d
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-22 21:49:12 +0530

    `/news` command added

--- Cutiepii_Robot/modules/memes.py
+++ Cutiepii_Robot/modules/memes.py
@@ -56,6 +56,30 @@ nltk.download("averaged_perceptron_tagger")
 WIDE_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
 WIDE_MAP[0x20] = 0x3000
 
+
+@pgram.on(events.NewMessage(pattern="^/news ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    infintyvar = event.pattern_match.group(1)
+    main_url = f"https://inshortsapi.vercel.app/news?category={infintyvar}"
+    stuber = await event.reply(
+        f"Ok ! Fectching {infintyvar} From inshortsapi Server And Sending To News Channel",
+    )
+    await stuber.edit("All News Has Been Sucessfully fetched, sendning to you.")
+    starknews = requests.get(main_url).json()
+    for item in starknews["data"]:
+        sedlyf = item["content"]
+        img = item["imageUrl"]
+        writter = item["author"]
+        dateis = item["date"]
+        readthis = item["readMoreUrl"]
+        titles = item["title"]
+        sed1 = img
+        sedm = f"**Title : {titles}** \n{sedlyf} \nDate : {dateis} \nAuthor : {writter} \nReadMore : {readthis}"
+        await pgram.send_file(event.chat_id, sed1, caption=sedm)
+        
+
 @register(pattern="^/ball(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
