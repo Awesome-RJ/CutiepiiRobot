d84f0f0ac466503aad384a1b65725457a545a4c7
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-28 14:19:13 +0530

    test

--- Cutiepii_Robot/modules/nhentai.py
+++ Cutiepii_Robot/modules/nhentai.py
@@ -55,6 +55,8 @@ async def nhentai(client, message):
             ]
         )
     )
+    except Exception:
+      return msg.reply_text("There was a problem while importing the data!")
 
 
 def nhentai_data(noombers):
