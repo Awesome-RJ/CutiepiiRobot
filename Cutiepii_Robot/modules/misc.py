3b8f8c4cfb17b31cce0c7092ef12a9fb41059c4d
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-11-03 11:42:10 +0530

    `Reddit Memes` Added

--- Cutiepii_Robot/modules/misc.py
+++ Cutiepii_Robot/modules/misc.py
@@ -60,17 +60,20 @@ Keep in mind that your message <b>MUST</b> contain some text other than just a b
 
 
 @user_admin
-def echo(update: Update, context: CallbackContext):
+def echo(update, _):
     args = update.effective_message.text.split(None, 1)
     message = update.effective_message
 
     if message.reply_to_message:
         message.reply_to_message.reply_text(
-            args[1], parse_mode="MARKDOWN", disable_web_page_preview=True,
+            args[1], parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
         )
     else:
         message.reply_text(
-            args[1], quote=False, parse_mode="MARKDOWN", disable_web_page_preview=True,
+            args[1],
+            quote=False,
+            parse_mode=ParseMode.MARKDOWN,
+            disable_web_page_preview=True,
         )
     message.delete()
 
@@ -87,7 +90,64 @@ def markdown_help_sender(update: Update):
     )
 
 
+@typing_action
+def src(update, _):
+    update.effective_message.reply_text(
+        "Hey there! You can find what makes me click [here](https://github.com/Awesome-RJ/CutiepiiRobot).",
+        parse_mode=ParseMode.MARKDOWN,
+        disable_web_page_preview=True,
+    )
+    
+@send_action(ChatAction.UPLOAD_PHOTO)
+def rmemes(update, context):
+    msg = update.effective_message
+    chat = update.effective_chat
 
+    SUBREDS = [
+        "meirl",
+        "dankmemes",
+        "AdviceAnimals",
+        "memes",
+        "meme",
+        "memes_of_the_dank",
+        "PornhubComments",
+        "teenagers",
+        "memesIRL",
+        "insanepeoplefacebook",
+        "terriblefacebookmemes",
+    ]
+
+    subreddit = random.choice(SUBREDS)
+    res = r.get(f"https://meme-api.herokuapp.com/gimme/{subreddit}")
+
+    if res.status_code != 200:  # Like if api is down?
+        msg.reply_text("Sorry some error occurred :(")
+        return
+    res = res.json()
+
+    rpage = res.get(str("subreddit"))  # Subreddit
+    title = res.get(str("title"))  # Post title
+    memeu = res.get(str("url"))  # meme pic url
+    plink = res.get(str("postLink"))
+
+    caps = f"× <b>Title</b>: {title}\n"
+    caps += f"× <b>Subreddit:</b> <pre>r/{rpage}</pre>"
+
+    keyb = [[InlineKeyboardButton(text="Subreddit Postlink 🔗", url=plink)]]
+    try:
+        context.bot.send_photo(
+            chat.id,
+            photo=memeu,
+            caption=caps,
+            reply_markup=InlineKeyboardMarkup(keyb),
+            timeout=60,
+            parse_mode=ParseMode.HTML,
+        )
+
+    except BadRequest as excp:
+        return msg.reply_text(f"Error! {excp.message}")
+   
+   
 def markdown_help(update: Update, context: CallbackContext):
     if update.effective_chat.type != "private":
         update.effective_message.reply_text(
@@ -197,13 +257,19 @@ Bass Boosting
 
 ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.chat_type.groups, run_async=True)
 MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help, run_async=True)
+SRC_HANDLER = CommandHandler("source", src, filters=Filters.chat_type.private, run_async=True)
+REDDIT_MEMES_HANDLER = DisableAbleCommandHandler("rmeme", rmemes, run_async=True)
 
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
+dispatcher.add_handler(SRC_HANDLER)
+dispatcher.add_handler(REDDIT_MEMES_HANDLER)
 
 __mod_name__ = "Extras"
-__command_list__ = ["id", "echo"]
+__command_list__ = ["id", "echo", "source", "rmeme"]
 __handlers__ = [
     ECHO_HANDLER,
     MD_HELP_HANDLER,
+    SRC_HANDLER,
+    REDDIT_MEMES_HANDLER,
 ]
