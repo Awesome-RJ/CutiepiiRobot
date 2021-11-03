0040730cb3b1e5b8c071139f63e61696f6c35c62
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-11-03 15:19:02 +0530

    Update gtranslator.py

--- Cutiepii_Robot/modules/gtranslator.py
+++ Cutiepii_Robot/modules/gtranslator.py
@@ -25,6 +25,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+import os
 
 from gpytranslate import SyncTranslator
 from telegram import Update,ChatAction, InlineKeyboardButton, InlineKeyboardMarkup
