ab8cc763941610e2b8213c93992ba2cac698c106
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2021-11-03 12:23:31 +0530

    | Yūki • Black Knights Union Project | PTB:- 13.7

--- Cutiepii_Robot/modules/karma.py
+++ Cutiepii_Robot/modules/karma.py
@@ -2,7 +2,9 @@
 """
 MIT License
 
+Copyright (C) 2017-2019, Paul Larsen
 Copyright (C) 2021 Awesome-RJ
+Copyright (c) 2021, Yūki • Black Knights Union, <https://github.com/Awesome-RJ/CutiepiiRobot>
 
 This file is part of @Cutiepii_Robot (Telegram Bot)
 
@@ -11,8 +13,8 @@ of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
 
+furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
