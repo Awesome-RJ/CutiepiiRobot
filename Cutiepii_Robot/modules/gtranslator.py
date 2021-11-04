969ddb9d3e3e69a436afe47fc9647a6ba8f5f962
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-11-05 02:49:53 +0530

    Update gtranslator.py

--- Cutiepii_Robot/modules/gtranslator.py
+++ Cutiepii_Robot/modules/gtranslator.py
@@ -26,6 +26,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 import os
+import gTTS
 
 from gpytranslate import SyncTranslator
 from telegram import Update, ChatAction, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
