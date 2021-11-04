74464cbe6a382e89ecc6069c85b084013f854ab6
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-11-04 20:51:42 +0530

    Update admin.py

--- Cutiepii_Robot/modules/admin.py
+++ Cutiepii_Robot/modules/admin.py
@@ -32,6 +32,7 @@ from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
+from telethon import events
 
 from Cutiepii_Robot import DRAGONS, dispatcher, telethn as bot
 from Cutiepii_Robot.modules.disable import DisableAbleCommandHandler
