d9b31c2dd0c7fd58eebd43b7036f149ea6269120
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-10 08:26:14 +0700

    fixed typo

--- Cutiepii_Robot/modules/tiny.py
+++ Cutiepii_Robot/modules/tiny.py
@@ -13,7 +13,7 @@ async def _(event):
            return
     kontol = await event.reply("`Processing tiny...`")
     ik = await tbot.download_media(reply)
-    im1 = Image.open("aries/resources/ken.png")
+    im1 = Image.open("Cutiepii_Robot/resources/ken.png")
     if ik.endswith(".tgs"):
         await tbot.download_media(reply, "ken.tgs")
         os.system("lottie_convert.py ken.tgs json.json")
