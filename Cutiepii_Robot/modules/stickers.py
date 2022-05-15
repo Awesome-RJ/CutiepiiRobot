0adc846e3c3bd2e40896f43d5a995ff1fc319843
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2022-05-15 00:09:59 -0700

    `urlquote` now defined

--- Cutiepii_Robot/modules/stickers.py
+++ Cutiepii_Robot/modules/stickers.py
@@ -12,6 +12,7 @@ from telegram import Update
 from telegram.ext import CallbackContext, CallbackQueryHandler
 from telegram.constants import ParseMode
 from telegram.helpers import mention_html
+from urllib.parse import quote as urlquote
 
 from Cutiepii_Robot import REDIS, CUTIEPII_PTB
 from Cutiepii_Robot.events import register as Cutiepii
