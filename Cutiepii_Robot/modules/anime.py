c48663b2246f38e71f1e5be1884ff15898b0eab8
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2022-05-15 00:45:48 -0700

    Update anime.py

--- Cutiepii_Robot/modules/anime.py
+++ Cutiepii_Robot/modules/anime.py
@@ -753,7 +753,7 @@ def button(update, context):
     query_type = data[0]
     original_user_id = int(data[1])
 
-    user_and_admin_list = [original_user_id, OWNER_ID] + DRAGONS + DEV_USERS
+    user_and_admin_list = [original_user_id, OWNER_ID] + SUDO_USERS + DEV_USERS
 
     bot.answer_callback_query(query.id)
     if query_type == "anime_close":
