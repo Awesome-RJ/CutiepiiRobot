1b66a7fc635d5ec66fd9abf02e9a22a99d5a43b1
Rajkumar Wadiwala <71130660+Awesome-RJ@users.noreply.github.com>
2022-05-27 15:54:33 +0530

    code compression (Handlers) | PTB:- 20.0a0

--- Cutiepii_Robot/modules/lewd.py
+++ Cutiepii_Robot/modules/lewd.py
@@ -682,175 +682,62 @@ async def baka(update: Update, context: CallbackContext):
 
 
 
-ADD_NSFW_HANDLER = CommandHandler("addnsfw", add_nsfw)
-REMOVE_NSFW_HANDLER = CommandHandler("rmnsfw", rem_nsfw)
-LIST_NSFW_CHATS_HANDLER = CommandHandler(
-    "nsfwchats", list_nsfw_chats, filters=CustomFilters.dev_filter)
-LEWDKEMO_HANDLER = CommandHandler("lewdkemo", lewdkemo)
-NEKO_HANDLER = CommandHandler("neko", neko)
-FEET_HANDLER = CommandHandler("feet", feet)
-YURI_HANDLER = CommandHandler("yuri", yuri)
-TRAP_HANDLER = CommandHandler("trap", trap)
-FUTANARI_HANDLER = CommandHandler("futanari", futanari)
-HOLOLEWD_HANDLER = CommandHandler("hololewd", hololewd)
-SOLOGIF_HANDLER = CommandHandler("sologif", sologif)
-CUMGIF_HANDLER = CommandHandler("cumgif", cumgif)
-EROKEMO_HANDLER = CommandHandler("erokemo", erokemo)
-LESBIAN_HANDLER = CommandHandler("lesbian", lesbian)
-WALLPAPER_HANDLER = CommandHandler("wallpaper", wallpaper)
-LEWDK_HANDLER = CommandHandler("lewdk", lewdk)
-NGIF_HANDLER = CommandHandler("ngif", ngif)
-TICKLE_HANDLER = CommandHandler("tickle", tickle)
-LEWD_HANDLER = CommandHandler("lewd", lewd)
-FEED_HANDLER = CommandHandler("feed", feed)
-EROYURI_HANDLER = CommandHandler("eroyuri", eroyuri)
-ERON_HANDLER = CommandHandler("eron", eron)
-CUM_HANDLER = CommandHandler("cum", cum)
-BJGIF_HANDLER = CommandHandler("bjgif", bjgif)
-BJ_HANDLER = CommandHandler("bj", bj)
-NEKONSFW_HANDLER = CommandHandler("nekonsfw", nekonsfw)
-SOLO_HANDLER = CommandHandler("solo", solo)
-KEMONOMIMI_HANDLER = CommandHandler("kemonomimi", kemonomimi)
-AVATARLEWD_HANDLER = CommandHandler("avatarlewd", avatarlewd)
-GASM_HANDLER = CommandHandler("gasm", gasm)
-POKE_HANDLER = CommandHandler("poke", poke)
-ANAL_HANDLER = CommandHandler("anal", anal)
-HENTAI_HANDLER = CommandHandler("hentai", hentai)
-AVATAR_HANDLER = CommandHandler("avatar", avatar)
-EROFEET_HANDLER = CommandHandler("erofeet", erofeet)
-HOLO_HANDLER = CommandHandler("holo", holo)
-TITS_HANDLER = CommandHandler("tits", tits)
-PUSSYGIF_HANDLER = CommandHandler("pussygif", pussygif)
-HOLOERO_HANDLER = CommandHandler("holoero", holoero)
-PUSSY_HANDLER = CommandHandler("pussy", pussy)
-HENTAIGIF_HANDLER = CommandHandler("hentaigif", hentaigif)
-CLASSIC_HANDLER = CommandHandler("classic", classic)
-KUNI_HANDLER = CommandHandler("kuni", kuni)
-WAIFU_HANDLER = CommandHandler("waifu", waifu)
-LEWD_HANDLER = CommandHandler("lewd", lewd)
-KISS_HANDLER = CommandHandler("kiss", kiss)
-FEMDOM_HANDLER = CommandHandler("femdom", femdom)
-CUDDLE_HANDLER = CommandHandler("cuddle", cuddle)
-HUG_HANDLER = CommandHandler("hug", hug)
-EROK_HANDLER = CommandHandler("erok", erok)
-FOXGIRL_HANDLER = CommandHandler("foxgirl", foxgirl)
-TITSGIF_HANDLER = CommandHandler("titsgif", titsgif)
-ERO_HANDLER = CommandHandler("ero", ero)
-SMUG_HANDLER = CommandHandler("smug", smug)
-BAKA_HANDLER = CommandHandler("baka", baka)
+CUTIEPII_PTB.add_handler(CommandHandler("addnsfw", add_nsfw, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("rmnsfw", rem_nsfw, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("nsfwchats", list_nsfw_chats, filters=CustomFilters.dev_filter, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("lewdkemo", lewdkemo, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("neko", neko, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("feet", feet, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("yuri", yuri, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("trap", trap, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("futanari", futanari, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("hololewd", hololewd, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("sologif", sologif, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("cumgif", cumgif, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("erokemo", erokemo, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("lesbian", lesbian, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("wallpaper", wallpaper, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("lewdk", lewdk, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("ngif", ngif, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("tickle", tickle, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("lewd", lewd, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("feed", feed, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("eroyuri", eroyuri, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("eron", eron, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("cum", cum, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("bjgif", bjgif, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("bj", bj, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("nekonsfw", nekonsfw, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("solo", solo, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("kemonomimi", kemonomimi, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("avatarlewd", avatarlewd, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("gasm", gasm, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("poke", poke, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("anal", anal, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("hentai", hentai, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("avatar", avatar, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("erofeet", erofeet, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("holo", holo, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("tits", tits, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("pussygif", pussygif, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("holoero", holoero, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("pussy", pussy, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("hentaigif", hentaigif, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("classic", classic, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("kuni", kuni, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("waifu", waifu, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("lewd", lewd, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("kiss", kiss, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("femdom", femdom, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("cuddle", cuddle, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("hug", hug, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("erok", erok, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("foxgirl", foxgirl, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("titsgif", titsgif, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("ero", ero, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("smug", smug, block=False))
+CUTIEPII_PTB.add_handler(CommandHandler("baka", baka, block=False))
 
-
-CUTIEPII_PTB.add_handler(ADD_NSFW_HANDLER)
-CUTIEPII_PTB.add_handler(REMOVE_NSFW_HANDLER)
-CUTIEPII_PTB.add_handler(LIST_NSFW_CHATS_HANDLER)
-CUTIEPII_PTB.add_handler(LEWDKEMO_HANDLER)
-CUTIEPII_PTB.add_handler(NEKO_HANDLER)
-CUTIEPII_PTB.add_handler(FEET_HANDLER)
-CUTIEPII_PTB.add_handler(YURI_HANDLER)
-CUTIEPII_PTB.add_handler(TRAP_HANDLER)
-CUTIEPII_PTB.add_handler(FUTANARI_HANDLER)
-CUTIEPII_PTB.add_handler(HOLOLEWD_HANDLER)
-CUTIEPII_PTB.add_handler(SOLOGIF_HANDLER)
-CUTIEPII_PTB.add_handler(CUMGIF_HANDLER)
-CUTIEPII_PTB.add_handler(EROKEMO_HANDLER)
-CUTIEPII_PTB.add_handler(LESBIAN_HANDLER)
-CUTIEPII_PTB.add_handler(WALLPAPER_HANDLER)
-CUTIEPII_PTB.add_handler(LEWDK_HANDLER)
-CUTIEPII_PTB.add_handler(NGIF_HANDLER)
-CUTIEPII_PTB.add_handler(TICKLE_HANDLER)
-CUTIEPII_PTB.add_handler(LEWD_HANDLER)
-CUTIEPII_PTB.add_handler(FEED_HANDLER)
-CUTIEPII_PTB.add_handler(EROYURI_HANDLER)
-CUTIEPII_PTB.add_handler(ERON_HANDLER)
-CUTIEPII_PTB.add_handler(CUM_HANDLER)
-CUTIEPII_PTB.add_handler(BJGIF_HANDLER)
-CUTIEPII_PTB.add_handler(BJ_HANDLER)
-CUTIEPII_PTB.add_handler(NEKONSFW_HANDLER)
-CUTIEPII_PTB.add_handler(SOLO_HANDLER)
-CUTIEPII_PTB.add_handler(KEMONOMIMI_HANDLER)
-CUTIEPII_PTB.add_handler(AVATARLEWD_HANDLER)
-CUTIEPII_PTB.add_handler(GASM_HANDLER)
-CUTIEPII_PTB.add_handler(POKE_HANDLER)
-CUTIEPII_PTB.add_handler(ANAL_HANDLER)
-CUTIEPII_PTB.add_handler(HENTAI_HANDLER)
-CUTIEPII_PTB.add_handler(AVATAR_HANDLER)
-CUTIEPII_PTB.add_handler(EROFEET_HANDLER)
-CUTIEPII_PTB.add_handler(HOLO_HANDLER)
-CUTIEPII_PTB.add_handler(TITS_HANDLER)
-CUTIEPII_PTB.add_handler(PUSSYGIF_HANDLER)
-CUTIEPII_PTB.add_handler(HOLOERO_HANDLER)
-CUTIEPII_PTB.add_handler(PUSSY_HANDLER)
-CUTIEPII_PTB.add_handler(HENTAIGIF_HANDLER)
-CUTIEPII_PTB.add_handler(CLASSIC_HANDLER)
-CUTIEPII_PTB.add_handler(KUNI_HANDLER)
-CUTIEPII_PTB.add_handler(WAIFU_HANDLER)
-CUTIEPII_PTB.add_handler(LEWD_HANDLER)
-CUTIEPII_PTB.add_handler(KISS_HANDLER)
-CUTIEPII_PTB.add_handler(FEMDOM_HANDLER)
-CUTIEPII_PTB.add_handler(CUDDLE_HANDLER)
-CUTIEPII_PTB.add_handler(EROK_HANDLER)
-CUTIEPII_PTB.add_handler(FOXGIRL_HANDLER)
-CUTIEPII_PTB.add_handler(TITSGIF_HANDLER)
-CUTIEPII_PTB.add_handler(ERO_HANDLER)
-CUTIEPII_PTB.add_handler(SMUG_HANDLER)
-CUTIEPII_PTB.add_handler(BAKA_HANDLER)
-
-__handlers__ = [
-    ADD_NSFW_HANDLER,
-    REMOVE_NSFW_HANDLER,
-    LIST_NSFW_CHATS_HANDLER,
-    NEKO_HANDLER,
-    FEET_HANDLER,
-    YURI_HANDLER,
-    TRAP_HANDLER,
-    FUTANARI_HANDLER,
-    HOLOLEWD_HANDLER,
-    SOLOGIF_HANDLER,
-    CUMGIF_HANDLER,
-    EROKEMO_HANDLER,
-    LESBIAN_HANDLER,
-    WALLPAPER_HANDLER,
-    LEWDK_HANDLER,
-    NGIF_HANDLER,
-    TICKLE_HANDLER,
-    LEWD_HANDLER,
-    FEED_HANDLER,
-    EROYURI_HANDLER,
-    ERON_HANDLER,
-    CUM_HANDLER,
-    BJGIF_HANDLER,
-    BJ_HANDLER,
-    NEKONSFW_HANDLER,
-    SOLO_HANDLER,
-    KEMONOMIMI_HANDLER,
-    AVATARLEWD_HANDLER,
-    GASM_HANDLER,
-    POKE_HANDLER,
-    ANAL_HANDLER,
-    HENTAI_HANDLER,
-    AVATAR_HANDLER,
-    EROFEET_HANDLER,
-    HOLO_HANDLER,
-    TITS_HANDLER,
-    PUSSYGIF_HANDLER,
-    HOLOERO_HANDLER,
-    PUSSY_HANDLER,
-    HENTAIGIF_HANDLER,
-    CLASSIC_HANDLER,
-    KUNI_HANDLER,
-    WAIFU_HANDLER,
-    LEWD_HANDLER,
-    KISS_HANDLER,
-    FEMDOM_HANDLER,
-    LEWDKEMO_HANDLER,
-    CUDDLE_HANDLER,
-    EROK_HANDLER,
-    FOXGIRL_HANDLER,
-    TITSGIF_HANDLER,
-    ERO_HANDLER,
-    SMUG_HANDLER,
-    BAKA_HANDLER,
-]
 
 __help__ = """
 Usage*:*
