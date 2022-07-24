6b55cf543216851d309ebb9b9ad2bfd5302318f1
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2022-07-24 20:51:51 +0530

    Update anonymous.py

--- Cutiepii_Robot/modules/helper_funcs/anonymous.py
+++ Cutiepii_Robot/modules/helper_funcs/anonymous.py
@@ -31,9 +31,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from enum import Enum
 import functools
 
-from telegram import Update, InlineKeyboardButton
-from telegram import InlineKeyboardMarkup
-from telegram.ext import ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler, ContextTypes
 from telegram.constants import ParseMode
 
 from Cutiepii_Robot import DEV_USERS, SUDO_USERS, CUTIEPII_PTB
