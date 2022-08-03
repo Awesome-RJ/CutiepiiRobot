c97330408f3e3ec34f5c550e44cda076861a6d0a
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2022-08-03 20:07:01 +0530

    error handling Fixed

--- Cutiepii_Robot/modules/helper_funcs/misc.py
+++ Cutiepii_Robot/modules/helper_funcs/misc.py
@@ -30,16 +30,24 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import contextlib
-import random
+import requests
+import json
+import zlib
+import base64
+import base58
 
-from typing import Dict, List, Optional
+from typing import Dict, List
 from asyncio import sleep
+from urllib.parse import urlparse, urljoin, urlunparse
+from Crypto import Random, Hash, Protocol
+from Crypto.Cipher import AES
 
 from Cutiepii_Robot import NO_LOAD
 from telegram import Bot, InlineKeyboardButton
 from telegram.error import TelegramError
 from telegram.constants import ParseMode, MessageLimit
 
+MAX_MESSAGE_LENGTH = MessageLimit.TEXT_LENGTH
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other):
@@ -59,14 +67,14 @@ def delete(delmsg, timer):
         return
 
 def split_message(msg: str) -> List[str]:
-    if len(msg) < MessageLimit.TEXT_LENGTH:
+    if len(msg) < MAX_MESSAGE_LENGTH:
         return [msg]
 
     lines = msg.splitlines(True)
     small_msg = ""
     result = []
     for line in lines:
-        if len(small_msg) + len(line) < MessageLimit.TEXT_LENGTH:
+        if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
             small_msg += line
         else:
             result.append(small_msg)
@@ -74,7 +82,7 @@ def split_message(msg: str) -> List[str]:
     # Else statement at the end of the for loop, so append the leftover string.
     result.append(small_msg)
 
-    return result
+    return 
 
 def paginate_modules(_: int, module_dict: Dict, prefix, chat=None) -> List[List[EqInlineKeyboardButton]]:
     modules = (
@@ -119,7 +127,7 @@ async def send_to_list(
     for user_id in set(send_to):
         with contextlib.suppress(TelegramError):
             if markdown:
-                await bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN_V2)
+                await bot.send_message(user_id, message, parse_mode=ParseMode.MARKDOWN)
             elif html:
                 await bot.send_message(user_id, message, parse_mode=ParseMode.HTML)
             else:
@@ -163,18 +171,49 @@ def is_module_loaded(name):
     return name not in NO_LOAD
 
 
-def upload_text(data: str) -> Optional[str]:
+def upload_text(data: str) -> typing.Optional[str]:
     passphrase = Random.get_random_bytes(32)
     salt = Random.get_random_bytes(8)
-    key = Protocol.KDF.PBKDF2(passphrase, salt, 32, 100000, hmac_hash_module=Hash.SHA256)
+    key = Protocol.KDF.PBKDF2(
+        passphrase, salt, 32, 100000, hmac_hash_module=Hash.SHA256
+    )
     compress = zlib.compressobj(wbits=-15)
-    paste_blob = compress.compress(json.dumps({'paste': data}, separators=(',', ':')).encode()) + compress.flush()
+    paste_blob = (
+        compress.compress(json.dumps({"paste": data}, separators=(",", ":")).encode())
+        + compress.flush()
+    )
     cipher = AES.new(key, AES.MODE_GCM)
-    paste_meta = [[base64.b64encode(cipher.nonce).decode(), base64.b64encode(salt).decode(), 100000, 256, 128, 'aes', 'gcm', 'zlib'], 'syntaxhighlighting', 0, 0]
-    cipher.update(json.dumps(paste_meta, separators=(',', ':')).encode())
+    paste_meta = [
+        [
+            base64.b64encode(cipher.nonce).decode(),
+            base64.b64encode(salt).decode(),
+            100000,
+            256,
+            128,
+            "aes",
+            "gcm",
+            "zlib",
+        ],
+        "syntaxhighlighting",
+        0,
+        0,
+    ]
+    cipher.update(json.dumps(paste_meta, separators=(",", ":")).encode())
     ct, tag = cipher.encrypt_and_digest(paste_blob)
-    resp = requests.post('https://bin.nixnet.services', headers={'X-Requested-With': 'JSONHttpRequest'}, data=json.dumps({'v': 2, 'adata': paste_meta, 'ct': base64.b64encode(ct + tag).decode(), 'meta': {'expire': '1week'}}, separators=(',', ':')))
+    resp = requests.post(
+        "https://bin.nixnet.services",
+        headers={"X-Requested-With": "JSONHttpRequest"},
+        data=json.dumps(
+            {
+                "v": 2,
+                "adata": paste_meta,
+                "ct": base64.b64encode(ct + tag).decode(),
+                "meta": {"expire": "1week"},
+            },
+            separators=(",", ":"),
+        ),
+    )
     data = resp.json()
-    url = list(urlparse(urljoin('https://bin.nixnet.services', data['url'])))
+    url = list(urlparse(urljoin("https://bin.nixnet.services", data["url"])))
     url[5] = base58.b58encode(passphrase).decode()
     return urlunparse(url)
