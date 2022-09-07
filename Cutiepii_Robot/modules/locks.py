314a9d4332e71f42a3e8953a0d34cecd4fe846b5
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2022-09-07 21:12:18 +0530

    kk

--- Cutiepii_Robot/modules/locks.py
+++ Cutiepii_Robot/modules/locks.py
@@ -23,7 +23,7 @@ from Cutiepii_Robot.modules.helper_funcs.chat_status import (
     is_bot_admin,
     user_admin as u_admin,
 )
-from Cutiepii_Robot.modules.helper_funcs.decorators import cutiepii_cmd, cutiepii_msg
+
 from Cutiepii_Robot.modules.log_channel import loggable
 from Cutiepii_Robot.modules.sql.approve_sql import is_approved
 from Cutiepii_Robot.modules.helper_funcs.anonymous import user_admin, AdminPerms
@@ -129,7 +129,7 @@ async def unrestr_members(
             )
 
 
-@cutiepii_cmd(command="locktypes")
+
 async def locktypes(update, _):
     await update.effective_message.reply_text(
         "\n • ".join(
@@ -139,7 +139,6 @@ async def locktypes(update, _):
     )
 
 
-@cutiepii_cmd(command="lock")
 @user_admin(AdminPerms.CAN_CHANGE_INFO)
 @loggable
 async def lock(update: Update, context: CallbackContext) -> str:  # sourcery no-metrics
@@ -244,7 +243,7 @@ async def lock(update: Update, context: CallbackContext) -> str:  # sourcery no-
     return ""
 
 
-@cutiepii_cmd(command="unlock")
+
 @user_admin(AdminPerms.CAN_CHANGE_INFO)
 @loggable
 async def unlock(
@@ -341,7 +340,6 @@ async def unlock(
     return ""
 
 
-@cutiepii_msg((filters.ALL & filters.ChatType.GROUPS), group=PERM_GROUP)
 @user_not_admin
 async def del_lockables(update, context):  # sourcery no-metrics
     chat = update.effective_chat  # type: Optional[Chat]
@@ -455,7 +453,6 @@ async def build_lock_message(chat_id):
     return res
 
 
-@cutiepii_cmd(command="locks")
 @u_admin
 async def list_locks(update, context):
     chat = update.effective_chat  # type: Optional[Chat]
@@ -515,3 +512,36 @@ def __migrate__(old_chat_id, new_chat_id):
 
 def __chat_settings__(chat_id, user_id):
     return asyncio.get_running_loop().run_until_complete(build_lock_message(chat_id))
+
+__help__ = """
+Do stickers annoy you? or want to avoid people sharing links? or pictures? \
+You're in the right place!
+The locks module allows you to lock away some common items in the \
+telegram world; the bot will automatically delete them!
+➛ /locktypes*:* Lists all possible locktypes
+*Admins only:*
+➛ /lock <type>*:* Lock items of a certain type (not available in private)
+➛ /unlock <type>*:* Unlock items of a certain type (not available in private)
+➛ /locks*:* The current list of locks in this chat.
+Locks can be used to restrict a group's users.
+eg:
+Locking urls will auto-delete all messages with urls, locking stickers will restrict all \
+non-admin users from sending stickers, etc.
+Locking bots will stop non-admins from adding bots to the chat.
+*Note:*
+ ➛ Unlocking permission *info* will allow members (non-admins) to change the group information, such as the description or the group name
+ ➛ Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
+"""
+
+__mod_name__ = "Locks"
+
+CUTIEPII_PTB.add_handler(DisableAbleCommandHandler("locktypes", locktypes))
+CUTIEPII_PTB.add_handler(CommandHandler("lock",
+                                        lock))  # , filters=Filters.group)
+CUTIEPII_PTB.add_handler(CommandHandler("unlock",
+                                        unlock))  # , filters=Filters.group)
+CUTIEPII_PTB.add_handler(CommandHandler(
+    "locks", list_locks))  # , filters=Filters.group)
+CUTIEPII_PTB.add_handler(
+    MessageHandler(filters.ALL & filters.ChatType.GROUPS, del_lockables),
+    PERM_GROUP)
